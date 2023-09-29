ca30575444b49640b52ce915f395786104843406
martie <andres-martinez@uiowa.edu>
2023-09-29 09:22:15 -0500

    add last dataset.

--- R/ExtractingData/PassaicRiver/CombinePassaicData.R
+++ R/ExtractingData/PassaicRiver/CombinePassaicData.R
@@ -11,6 +11,7 @@ pass.6 <- read.csv("Data/PassaicRiver/2012 CPG CWCM Sampling - Round 5FV.csv")
 pass.7 <- read.csv("Data/PassaicRiver/2013 CPG CWCM Sampling - High Flow 1FV.csv")
 pass.8 <- read.csv("Data/PassaicRiver/2013 CPG CWCM Sampling - High Flow 2FV.csv")
 pass.9 <- read.csv("Data/PassaicRiver/2017-2019_OU2_PDI_Water_Column_20210924FV.csv")
+pass.10 <- read.csv("Data/PassaicRiver/2007-08 USEPA-MPI.csv")
 
 merged_pass <- rbind(pass.1, pass.2, pass.3, pass.4, pass.5, pass.6, pass.7,
                    pass.8, pass.9)
