91a7a1815c203870e206a4d0b84311ab3888facd
martie <andres-martinez@uiowa.edu>
2023-10-04 17:22:34 -0500

    new version. It is working but need more work.

--- R/ExtractingData/LMMB/LMMBED.R
+++ R/ExtractingData/LMMB/LMMBED.R
@@ -19,53 +19,91 @@ install.packages("lubridate")
 # Read the CSV file into a data frame
 LMMB_data <- read_csv("Data/LMMB/results.csv")
 
-# Remove the STAT_TYPE column
+# Check issues with the data before any modifications
+data_problems <- problems(LMMB_data)
+
+# Display data problems, if any
+if (nrow(data_problems) > 0) {
+  print(data_problems)
+} else {
+  cat("No initial data problems found.\n")
+}
+
+# Fix issues
+# Remove asterisks from all cells in the data frame
+LMMB_data <- data.frame(lapply(LMMB_data, function(x) gsub("\\*", "", x)))
+
+# Make values to numeric values
+# Select columns that start with "VALUE"
+value_columns <- grep("^VALUE", names(LMMB_data), value = TRUE)
+
+# Check if warnings exist and clear them
+if (length(warnings()) > 0) {
+  warnings()
+  warnings(NULL)
+}
+
+# Check issues with the data before any modifications
+data_problems <- problems(LMMB_data)
+
+# Transform non-numeric columns to numeric values
 LMMB_data <- LMMB_data %>%
-  select(-Row, -STAT_TYPE)
+  mutate_at(vars(all_of(value_columns)), ~{
+    numeric_value <- as.numeric(as.character(.))
+    ifelse(is.na(numeric_value) | is.infinite(numeric_value), NA, numeric_value)
+  })
 
-# Filter rows based on the SAMPLE_TYPE condition, removing QC samples
+# Check for problems in the dataframe
+data_problems <- problems(LMMB_data)
+
+# Print the rows and columns with problems
+print(data_problems)
+
+# No problems, but still some columns that need to be numeric are not numeric
+# Check the format of each selected column
+format_info <- sapply(LMMB_data[, value_columns], class)
+print(format_info)
+
+# Select columns that need fixing
+columns_to_fix <- c("VALUE_123", "VALUE_124", "VALUE_125", "VALUE_126", "VALUE_127")
+
+# Loop through the selected columns and convert problematic values
+for (col in columns_to_fix) {
+  LMMB_data[[col]] <- as.numeric(as.character(LMMB_data[[col]]))
+  # Identify non-numeric values and replace them with NA
+  LMMB_data[[col]][!is.na(LMMB_data[[col]]) & !is.finite(LMMB_data[[col]])] <- NA
+}
+
+# Verify that the columns are now numeric
+str(LMMB_data[, columns_to_fix])
+
+# Work on the reshaping the data
+# Remove the "STAT_TYPE" column
 LMMB_data <- LMMB_data %>%
-  filter(SAMPLE_TYPE == "Individual")
+  select(-STAT_TYPE)
 
-# Convert the datetime column to a DateTime object
-LMMB_data$SAMPLING_DATE <- mdy_hm(LMMB_data$SAMPLING_DATE)
+# Identify the range of columns for chemical data (columns 20 to 781)
+pivot_columns <- names(LMMB_data)[20:781]
 
-# Format the datetime column as "mm/dd/yyyy"
-LMMB_data$SAMPLING_DATE <- format(LMMB_data$SAMPLING_DATE, format = "%m/%d/%Y")
+# Pivot the data while keeping metadata columns and maintaining the "ANAL_CODE" column
+LMMB_data_long <- LMMB_data %>%
+  pivot_longer(
+    cols = all_of(pivot_columns),
+    names_to = c(".value", "Chemical"),
+    names_sep = "_"
+  ) %>%
+  filter(!is.na(VALUE))  # Remove rows with NA values in VALUE column
 
-# Select the metadata columns (from 'PROJECT' to 'SAMPLE_ID')
-metadata <- LMMB_data %>%
-  select(PROJECT:SAMPLE_ID)
+# Remove a few columns not needed.
+LMMB_data_long <- LMMB_data_long %>%
+  select(-Row, -PROJECT, -PROJ_CODE, -YEAR, -MONTH, -SEASON, -CRUISE_ID,
+         -TIME_ZONE, -STN_DEPTH_M, -SAMPLE_DEPTH_M, -VISIT_ID, -Chemical, -ANL, -RESULT)
 
+LMMB_data_long <- LMMB_data_long %>%
+  filter(FRACTION == "Filtrate", SAMPLE_TYPE == "Individual", UNITS == "ng/l")
 
-
-# Combine all chemical columns into a single dataset
-chemical_data <- LMMB_data %>%
-  select(ANL_CODE_1:`...782`)  # Adjust column names as needed
-
-
-# Assuming you have 'metadata' and 'chemical_data' data frames as defined earlier
-
-# Combine metadata and chemical data by adding a unique ID to both data frames
-metadata$ID <- seq_len(nrow(metadata))
-chemical_data$ID <- seq_len(nrow(chemical_data))
-
-# Split the chemical data into groups of 6 columns each (excluding ID)
-num_chemicals <- 6
-chemical_data_list <- lapply(seq(2, ncol(chemical_data), by = num_chemicals), function(start_col) {
-  end_col <- min(start_col + num_chemicals - 1, ncol(chemical_data))
-  select(chemical_data, starts_with("ANL_CODE_"))[, start_col:end_col, drop = FALSE]
-})
+# Change date format
+LMMB_data_long$SAMPLING_DATE <- sub(" \\d+:\\d+", "", LMMB_data_long$SAMPLING_DATE)
 
 
 
-# Combine the split chemical data into a single data frame
-chemical_data_combined <- do.call(cbind, chemical_data_list)
-
-# Merge metadata and combined chemical data using the 'ID' column
-final_data <- merge(metadata, chemical_data_combined, by = "ID")
-
-# Remove the 'ID' column
-final_data <- final_data[, -which(names(final_data) == "ID")]
-
-# Now, 'final_data' contains the metadata and chemical data with 6 columns each.
