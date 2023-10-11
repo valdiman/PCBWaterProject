e5699b164730fce20a59e9fb2d3add9e2792a21f
martie <andres-martinez@uiowa.edu>
2023-10-11 16:26:01 -0500

    working, but it doesn't do the congener list modification.

--- R/ExtractingData/LMMB/LMMB1994SpringTributary.R
+++ R/ExtractingData/LMMB/LMMB1994SpringTributary.R
@@ -6,7 +6,7 @@ install.packages("tidyr")
 install.packages("readr")
 install.packages("jsonlite")
 install.packages("lubridate")
-install.packages("stringr")
+install.packages("strings")
 
 # Load libraries
 {
@@ -17,7 +17,6 @@ install.packages("stringr")
   library(lubridate)
   library(stringr)
 }
-
 
 # Read the CSV file into a data frame
 LMMB_data <- read_csv("Data/LMMB/1994SpringTributary.csv")
@@ -61,56 +60,147 @@ LMMB_data_long <- LMMB_data_long %>%
          !grepl("^field blank", QC_TYPE),
          !grepl("^Composite QC", SAMPLE_TYPE))
 
-# Remove the "Chemical" column
+# Remove the metadata column not necessaries
 LMMB_data_long <- LMMB_data_long %>%
   select(-Row, -PROJECT, -PROJ_CODE, -YEAR, -MONTH, -SEASON,
          -CRUISE_ID, -VISIT_ID, -STATION_ID, -TIME_ZONE,
          -STN_DEPTH_M, -SAMPLE_DEPTH_M, -SAMPLE_TYPE, -QC_TYPE,
          -Chemical, -FRACTION, -RESULT_REMARK)
 
+# Remove PCB congeners with 2 measurements (both measurements are the same)
+LMMB_data_long <- LMMB_data_long %>%
+  group_by(LATITUDE, LONGITUDE, SAMPLING_DATE, MEDIUM, SAMPLE_ID, ANL_CODE, ANALYTE) %>%
+  mutate(VALUE = ifelse(row_number() == 1, VALUE, NA)) %>%
+  ungroup() %>%
+  na.omit()
+
 # Change date format
 LMMB_data_long$SAMPLING_DATE <- sub(" \\d+:\\d+", "",
                                     LMMB_data_long$SAMPLING_DATE)
 
-# Step 1: Extract into separate columns
-LMMB_data_long <- LMMB_data_long %>%
-  separate(ANL_CODE, into = c("Prefix", "FirstNum", "SecondNum", "ThirdNum"), sep = "_|\\+")
+# Function to change PCB names
+transform_ANL_CODE <- function(anl_code) {
+  # Replace "PCB-" with "PCB"
+  anl_code <- gsub("^PCB-", "PCB", anl_code)
+  
+  # Replace "PCB" with "PCB"
+  anl_code <- gsub("^PCB", "PCB", anl_code)
+  
+  # Extract numbers using regular expressions
+  numbers <- str_extract_all(anl_code, "\\d+")
+  
+  if (length(numbers) > 0) {
+    # Sort numbers in ascending order
+    sorted_numbers <- sort(as.integer(unlist(numbers)))
+    
+    # Replace dots with plus signs and combine sorted numbers
+    transformed_code <- paste("PCB", gsub("\\.", "+", paste(sorted_numbers, collapse = ".")), sep = "")
+  } else {
+    transformed_code <- anl_code
+  }
+  
+  return(transformed_code)
+}
 
-# Step 2: Remove leading zeros
-LMMB_data_long$FirstNum <- sub("^0+", "", LMMB_data_long$FirstNum)
-LMMB_data_long$SecondNum <- sub("^0+", "", LMMB_data_long$SecondNum)
-LMMB_data_long$ThirdNum <- sub("^0+", "", LMMB_data_long$ThirdNum)
-
-# Step 3: Replace NAs with empty strings for all numeric columns
-numeric_cols <- c("FirstNum", "SecondNum", "ThirdNum")
-LMMB_data_long[numeric_cols] <- lapply(LMMB_data_long[numeric_cols], function(x) ifelse(is.na(x), "", x))
-
-# Step 4: Concatenate with a dot, considering all numbers
-LMMB_data_long$Modified_ANL_CODE <- paste0(LMMB_data_long$Prefix,
-                                           ifelse(LMMB_data_long$FirstNum == "", "", LMMB_data_long$FirstNum),
-                                           ifelse(LMMB_data_long$SecondNum == "", "", paste0(".", LMMB_data_long$SecondNum)),
-                                           ifelse(LMMB_data_long$ThirdNum == "", "", paste0(".", LMMB_data_long$ThirdNum)))
-
-# Replace "PCB-tot" with "tPCB" in Modified_ANL_CODE column
-LMMB_data_long$Modified_ANL_CODE <- gsub("PCB-tot", "tPCB", LMMB_data_long$Modified_ANL_CODE)
-
-# Remove temporary columns if needed
-LMMB_data_long <- LMMB_data_long %>%
-  select(-Prefix, -FirstNum, -SecondNum, -ThirdNum,
-         -ANALYTE)
-
-
-
-
-# Read the JSON file with new congener list from code NewPCBList.R
-pcb_groups <- read_json("Data/pcb_groups.json")
-
-
+# Apply function to change the PCB names
+LMMB_data_long$Transformed_ANL_CODE <- sapply(LMMB_data_long$ANL_CODE,
+                                              transform_ANL_CODE)
 
 # Reshape the dataset
 transposed_data <- LMMB_data_long %>%
   pivot_wider(
     id_cols = c(LATITUDE, LONGITUDE, SAMPLING_DATE, SAMPLE_ID, UNITS),
-    names_from = ANALYTE,
-    values_from = AVG_VALUE
+    names_from = Transformed_ANL_CODE,
+    values_from = VALUE
   )
+
+# Multiply PCB columns by 1000
+pcb_columns <- names(transposed_data)[grep("^PCB\\d+", names(transposed_data))]
+transposed_data[, pcb_columns] <- transposed_data[, pcb_columns] * 1000
+
+# Multiply all values in the "PCB" column by 1000
+transposed_data$PCB <- transposed_data$PCB * 1000
+
+# Update RESULT_UNIT to "pg/l"
+transposed_data$UNITS <- "pg/l"
+
+# Rename the "PCB" column to "tPCB"
+colnames(transposed_data)[colnames(transposed_data) == "PCB"] <- "tPCB"
+
+# Reorder the columns so that "tPCB" is at the end
+transposed_data <- transposed_data %>%
+  select(-tPCB, everything())
+
+# Export results
+write.csv(transposed_data, file = "Data/LMMB/1994ST.csv")
+
+
+## Not sure about this in this code
+# Define a mapping between codes and names
+code_to_name <- c(
+  "TFOXRB01" = "Tributary Fox River",
+  "TFOXRB02" = "Tributary Fox River",
+  "TFOXRB03" = "Tributary Fox River",
+  "TFOXRB04" = "Tributary Fox River",
+  "TFOXRB05" = "Tributary Fox River",
+  "TGRANH01" = "Tributary Grand River",
+  "TGRANH02" = "Tributary Grand River",
+  "TGRANH03" = "Tributary Grand River",
+  "TGRANH04" = "Tributary Grand River",
+  "TGRANH05" = "Tributary Grand River",
+  "TGRANH06" = "Tributary Grand River",
+  "TGRANH07" = "Tributary Grand River",
+  "TGRANH08" = "Tributary Grand River",
+  "TKALAG01" = "Tributary Kalamazoo River",
+  "TKALAG02" = "Tributary Kalamazoo River",
+  "TKALAG03" = "Tributary Kalamazoo River",
+  "TKALAG05" = "Tributary Kalamazoo River",
+  "TKALAG06" = "Tributary Kalamazoo River",
+  "TMANIK01" = "Tributary Manistique River",
+  "TMANIK02" = "Tributary Manistique River",
+  "TMANIK03" = "Tributary Manistique River",
+  "TMANIK04" = "Tributary Manistique River",
+  "TMANIK05" = "Tributary Manistique River",
+  "TMANIK06" = "Tributary Manistique River",
+  "TMENOA01" = "Tributary Menominee River",
+  "TMENOA02" = "Tributary Menominee River",
+  "TMENOA03" = "Tributary Menominee River",
+  "TMENOA04" = "Tributary Menominee River",
+  "TMILWD01" = "Tributary Milwaukee River",
+  "TMILWD02" = "Tributary Milwaukee River",
+  "TMILWD03" = "Tributary Milwaukee River",
+  "TMILWD04" = "Tributary Milwaukee River",
+  "TMUSKI01" = "Tributary Muskegon River",
+  "TMUSKI02" = "Tributary Muskegon River",
+  "TMUSKI03" = "Tributary Muskegon River",
+  "TMUSKI04" = "Tributary Muskegon River",
+  "TMUSKI05" = "Tributary Muskegon River",
+  "TPEREJ01" = "Tributary Pere Marquette River",
+  "TPEREJ02" = "Tributary Pere Marquette River",
+  "TPEREJ03" = "Tributary Pere Marquette River",
+  "TPEREJ04" = "Tributary Pere Marquette River",
+  "TPEREJ05" = "Tributary Pere Marquette River",
+  "TSHEBC01" = "Tributary Sheboygan River",
+  "TSHEBC02" = "Tributary Sheboygan River",
+  "TSHEBC03" = "Tributary Sheboygan River",
+  "TSHEBC04" = "Tributary Sheboygan River",
+  "TSHEBC06" = "Tributary Sheboygan River",
+  "TSTJOF01" = "Tributary St. Joseph River",
+  "TSTJOF02" = "Tributary St. Joseph River",
+  "TSTJOF03" = "Tributary St. Joseph River",
+  "TSTJOF04" = "Tributary St. Joseph River",
+  "TSTJOF05" = "Tributary St. Joseph River"
+)
+
+# Create SiteName column
+transposed_data <- transposed_data %>%
+  mutate(SiteName = code_to_name[SAMPLE_ID])
+
+# Create SiteID Column
+transposed_data <- transposed_data %>%
+  mutate(SiteID = sprintf("WCPCB-LMM%03d",
+                          as.integer(factor(SiteName, levels = unique(SiteName)))))
+         
+
+
+
