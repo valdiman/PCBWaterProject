2ccbfbdbb71414eb881cb9b769fa75614a795b19
martie <andres-martinez@uiowa.edu>
2023-10-20 14:10:33 -0500

    added lines to get the google earth locations with a klm file

--- R/Sites/Analysis/NewBedford.R
+++ R/Sites/Analysis/NewBedford.R
@@ -27,8 +27,11 @@ install.packages("lmerTest")
 install.packages("zoo")
 install.packages("dataRetrieval")
 install.packages("reshape")
+install.packages("tidyr")
 install.packages('patchwork')
 install.packages("scales")
+install.packages("sf")
+install.packages("sfheaders")
 
 # Load libraries
 {
@@ -44,7 +47,10 @@ install.packages("scales")
   library(zoo) # yields seasons
   library(dataRetrieval) # read data from USGS
   library(reshape)
+  library(tidyr) # function gather
   library(patchwork) # combine plots
+  library(sf) # Create file to be used in Google Earth
+  library(sfheaders) # Create file to be used in Google Earth
 }
 
 # Read data ---------------------------------------------------------------
@@ -76,10 +82,18 @@ nbh <- wdc[str_detect(wdc$LocationName, 'New Bedford'),]
 }
 
 # Get coordinates per site to plot in Google Earth
-nbh.location <- nbh.tpcb[c('SiteID', 'Latitude', 'Longitude', 'tPCB')]
+location <- nbh.tpcb[c('SiteID', 'Latitude', 'Longitude', 'tPCB')]
 # Average tPCB per site
-nbh.location <- aggregate(tPCB ~ SiteID + Latitude + Longitude,
-                          data = nbh.location, mean)
+location <- aggregate(tPCB ~ SiteID + Latitude + Longitude,
+                      data = location, mean)
+# Create an sf data frame
+sf_location <- st_as_sf(location, coords = c("Longitude", "Latitude"))
+# Set the CRS to WGS 84 (EPSG:4326)
+sf_location <- st_set_crs(sf_location, 4326)
+# Define the full file path for the KML file
+kmlFilePath <- "Output/Data/Sites/GoogleEarth/NewBedfordHarborLocations.kml"
+# Write the KML file to the specified directory
+st_write(sf_location, kmlFilePath, driver = "kml", append = FALSE)
 
 # General plots -------------------------------------------------------------------
 # (1) Histograms
