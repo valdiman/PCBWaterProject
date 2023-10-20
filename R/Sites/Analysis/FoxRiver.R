0d5e98e99b965bc30254b78e58c0dce02dd04f62
martie <andres-martinez@uiowa.edu>
2023-10-20 14:09:47 -0500

    added lines to get the google earth locations with a klm file

--- R/Sites/Analysis/FoxRiver.R
+++ R/Sites/Analysis/FoxRiver.R
@@ -18,6 +18,8 @@ install.packages("dataRetrieval")
 install.packages("reshape")
 install.packages('patchwork')
 install.packages("scales")
+install.packages("sf")
+install.packages("sfheaders")
 
 # Load libraries
 {
@@ -34,6 +36,8 @@ install.packages("scales")
   library(dataRetrieval) # read data from USGS
   library(reshape)
   library(patchwork) # combine plots
+  library(sf) # Create file to be used in Google Earth
+  library(sfheaders) # Create file to be used in Google Earth
 }
 
 # Read data ---------------------------------------------------------------
@@ -65,10 +69,18 @@ fox <- wdc[str_detect(wdc$LocationName, 'Fox River'),]
 }
 
 # Get coordinates per site to plot in Google Earth
-fox.location <- fox.tpcb[c('SiteID', 'Latitude', 'Longitude', 'tPCB')]
+location <- fox.tpcb[c('SiteID', 'Latitude', 'Longitude', 'tPCB')]
 # Average tPCB per site
-fox.location <- aggregate(tPCB ~ SiteID + Latitude + Longitude,
-                            data = fox.location, mean)
+location <- aggregate(tPCB ~ SiteID + Latitude + Longitude,
+                      data = location, mean)
+# Create an sf data frame
+sf_location <- st_as_sf(location, coords = c("Longitude", "Latitude"))
+# Set the CRS to WGS 84 (EPSG:4326)
+sf_location <- st_set_crs(sf_location, 4326)
+# Define the full file path for the KML file
+kmlFilePath <- "Output/Data/Sites/GoogleEarth/FoxRiverLocations.kml"
+# Write the KML file to the specified directory
+st_write(sf_location, kmlFilePath, driver = "kml", append = FALSE)
 
 # General plots -------------------------------------------------------------------
 # (1) Histograms
