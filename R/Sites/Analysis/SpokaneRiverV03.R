5a82a4ca7de7855d79124fe7237a15f62a301c10
martie <andres-martinez@uiowa.edu>
2023-11-30 13:23:58 -0600

    Inital version using radom forest model.

---
+++ R/Sites/Analysis/SpokaneRiverV03.R
@@ -0,0 +1,297 @@
+## Water PCB concentrations data analysis per site
+## Spokane River
+## Random forest model
+
+# Install packages
+install.packages("randomForest")
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("robustbase")
+install.packages("dplyr")
+install.packages("tibble")
+install.packages("Matrix")
+install.packages("zoo")
+install.packages("dataRetrieval")
+install.packages("reshape")
+install.packages("tidyr")
+install.packages('patchwork')
+install.packages("scales")
+install.packages("sf")
+install.packages("units")
+install.packages("sfheaders")
+
+# Load libraries
+{
+  library(ggplot2)
+  library(scales) # function trans_breaks
+  library(stringr) # str_detect
+  library(robustbase) # function colMedians
+  library(dplyr) # performs %>%
+  library(tibble) # adds a column
+  library(zoo) # yields seasons
+  library(dataRetrieval) # read data from USGS
+  library(reshape)
+  library(tidyr) # function gather
+  library(patchwork) # combine plots
+  library(sf) # Create file to be used in Google Earth
+  library(units)
+  library(randomForest)
+}
+
+# Read data ---------------------------------------------------------------
+# Data in pg/L
+wdc <- read.csv("Data/WaterDataCongenerAroclor09072023.csv")
+
+# Select Spokane River data ---------------------------------------------------
+spo <- wdc[str_detect(wdc$LocationName, 'Spokane River'),]
+
+# Data preparation --------------------------------------------------------
+{
+  # Change date format
+  spo$SampleDate <- as.Date(spo$SampleDate, format = "%m/%d/%y")
+  # Calculate sampling time
+  time.day <- data.frame(as.Date(spo$SampleDate) - min(as.Date(spo$SampleDate)))
+  # Create individual code for each site sampled
+  site.numb <- spo$SiteID %>% as.factor() %>% as.numeric
+  # Include season
+  yq.s <- as.yearqtr(as.yearmon(spo$SampleDate, "%m/%d/%Y") + 1/12)
+  season.s <- factor(format(yq.s, "%q"), levels = 1:4,
+                     labels = c("0", "S-1", "S-2", "S-3")) # winter, spring, summer, fall
+  # Create data frame
+  spo.tpcb <- cbind(factor(spo$SiteID), spo$SampleDate,
+                    spo$Latitude, spo$Longitude, as.matrix(spo$tPCB),
+                    data.frame(time.day), site.numb, season.s)
+  # Add column names
+  colnames(spo.tpcb) <- c("SiteID", "date", "Latitude", "Longitude",
+                          "tPCB", "time", "site.code", "season")
+}
+
+# Include USGS flow data --------------------------------------------------
+# Include flow data from USGS station Spokane River
+{
+  siteSpoN1 <- "12417650" # SPOKANE RIVER BLW BLACKWELL NR COEUR D ALENE ID
+  siteSpoN2 <- "12419000" # Spokane River near Post Falls, ID
+  siteSpoN3 <- "12422500" # Spokane River at Spokane, WA
+  siteSpoN4 <- "12424000" # Hangman Creek at Spokane, WA
+  # Codes to retrieve data
+  paramflow <- "00060" # discharge, ft3/s
+  #paramtemp <- "00010" # water temperature, C No data
+  # Retrieve USGS data
+  flow.1 <- readNWISdv(siteSpoN1, paramflow,
+                       min(spo.tpcb$date), max(spo.tpcb$date))
+  flow.2 <- readNWISdv(siteSpoN2, paramflow,
+                       min(spo.tpcb$date), max(spo.tpcb$date))
+  flow.3 <- readNWISdv(siteSpoN3, paramflow,
+                       min(spo.tpcb$date), max(spo.tpcb$date))
+  flow.4 <- readNWISdv(siteSpoN4, paramflow,
+                       min(spo.tpcb$date), max(spo.tpcb$date))
+  
+  # Add USGS data to spo.tpcb, matching dates
+  spo.tpcb$flow.1 <- 0.03*flow.1$X_00060_00003[match(spo.tpcb$date,
+                                                     flow.1$Date)]
+  spo.tpcb$flow.2 <- 0.03*flow.2$X_00060_00003[match(spo.tpcb$date,
+                                                     flow.2$Date)]
+  spo.tpcb$flow.3 <- 0.03*flow.3$X_00060_00003[match(spo.tpcb$date,
+                                                     flow.3$Date)]
+  spo.tpcb$flow.4 <- 0.03*flow.4$X_00060_00003[match(spo.tpcb$date,
+                                                     flow.4$Date)]
+}
+
+# Remove site -------------------------------------------------------------
+## Sample sites not located at the Spokane River
+{
+  spo.tpcb.1 <- subset(spo.tpcb, SiteID != c("WCPCB-SPR002")) # City of Spokane WRF
+  spo.tpcb.1 <- subset(spo.tpcb.1, SiteID != c("WCPCB-SPR005")) # Regional WRF
+  spo.tpcb.1 <- subset(spo.tpcb.1, SiteID != c("WCPCB-SPR006")) # Inland Empire paper
+  spo.tpcb.1 <- subset(spo.tpcb.1, SiteID != c("WCPCB-SPR008")) # Kaiser Aluminum
+  spo.tpcb.1 <- subset(spo.tpcb.1, SiteID != c("WCPCB-SPR010")) # Liberty Lake sewer
+  spo.tpcb.1 <- subset(spo.tpcb.1, SiteID != c("WCPCB-SPR011")) # Post Falls WWTP
+  spo.tpcb.1 <- subset(spo.tpcb.1, SiteID != c("WCPCB-SPR013")) # Coeur d'Alene WWTP
+  spo.tpcb.1 <- subset(spo.tpcb.1, SiteID != c("WCPCB-SPR015")) # Hangman Creek
+}
+
+# Random Forest Model -----------------------------------------------------
+# Using all the data, spo.tpcb
+# Train-Test Split
+set.seed(123)
+train_indices <- sample(1:nrow(spo.tpcb), 0.8 * nrow(spo.tpcb))
+train_data <- spo.tpcb[train_indices, ]
+test_data <- spo.tpcb[-train_indices, ]
+
+# Fit the Model (1)
+rf_model.1 <- randomForest(log10(tPCB) ~ time + site.code + season + flow.1 +
+                             flow.2 + flow.3 + flow.4, data = train_data)
+
+# Make Predictions
+predictions.1 <- predict(rf_model.1, newdata = test_data)
+
+# Evaluate Model Performance
+mse.1 <- mean((predictions.1 - log10(test_data$tPCB))^2)
+rmse.1 <- sqrt(mse.1)
+mae.1 <- mean(abs(predictions.1 - log10(test_data$tPCB)))
+r_squared.1 <- 1 - (sum((log10(test_data$tPCB) - predictions.1)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
+
+# Feature Importance
+importance.1 <- importance(rf_model.1)
+# Plot features
+barplot(importance.1[, 1], names.arg = rownames(importance.1),
+        main = "Feature Importance", las = 2, cex.names = 0.7)
+
+# Create a data frame for plotting
+plot_data.1 <- data.frame(
+  Actual = log10(test_data$tPCB),
+  Predicted = predictions.1
+)
+
+# Create the scatter plot using ggplot2
+ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
+  geom_point(shape = 21, size = 3, fill = "white") +
+  scale_y_log10(limits = c(10, 10^5),
+                breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  scale_x_log10(limits = c(10, 10^5),
+                breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  xlab(expression(bold("Observed concentration " *Sigma*"PCB (pg/L)"))) +
+  ylab(expression(bold("Predicted lme concentration " *Sigma*"PCB (pg/L)"))) +
+  geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
+  geom_abline(intercept = 0.30103, slope = 1, col = "blue",
+              linewidth = 0.7) + # 1:2 line (factor of 2)
+  geom_abline(intercept = -0.30103, slope = 1, col = "blue",
+              linewidth = 0.7) + # 2:1 line (factor of 2)
+  theme_bw() +
+  theme(aspect.ratio = 15/15) +
+  annotation_logticks(sides = "bl")
+
+# Estimate a factor of 2 between observations and predictions
+# Create a data frame with observed and predicted values
+compare_df.1 <- data.frame(observed = test_data$tPCB,
+                           predicted = 10^predictions.1)
+
+# Estimate a factor of 2 between observations and predictions
+compare_df.1$factor2 <- compare_df.1$observed/compare_df.1$predicted
+
+# Calculate the percentage of observations within the factor of 2
+factor2_percentage.1 <- nrow(compare_df.1[compare_df.1$factor2 > 0.5 & compare_df.1$factor2 < 2, ])/nrow(compare_df.1)*100
+
+# Model (2)
+# Using pass.tpcb.1
+# Train-Test Split
+set.seed(123)
+train_indices <- sample(1:nrow(spo.tpcb.1), 0.8 * nrow(spo.tpcb.1))
+train_data <- spo.tpcb.1[train_indices, ]
+test_data <- spo.tpcb.1[-train_indices, ]
+
+# Fit the Model (1)
+rf_model.2 <- randomForest(log10(tPCB) ~ time + site.code + season + flow.1 +
+                             flow.2 + flow.3 + flow.4, data = train_data)
+
+# Make Predictions
+predictions.2 <- predict(rf_model.2, newdata = test_data)
+
+# Evaluate Model Performance
+mse.2 <- mean((predictions.2 - log10(test_data$tPCB))^2)
+rmse.2 <- sqrt(mse.2)
+mae.2 <- mean(abs(predictions.2 - log10(test_data$tPCB)))
+r_squared.2 <- 1 - (sum((log10(test_data$tPCB) - predictions.2)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
+
+# Feature Importance
+importance.2 <- importance(rf_model.2)
+# Plot features
+barplot(importance.2[, 1], names.arg = rownames(importance.2),
+        main = "Feature Importance", las = 2, cex.names = 0.7)
+
+# Create a data frame for plotting
+plot_data.2 <- data.frame(
+  Actual = log10(test_data$tPCB),
+  Predicted = predictions.2
+)
+
+# Create the scatter plot using ggplot2
+ggplot(plot_data.2, aes(x = 10^(Actual), y = 10^(Predicted))) +
+  geom_point(shape = 21, size = 3, fill = "white") +
+  scale_y_log10(limits = c(10, 10^4),
+                breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  scale_x_log10(limits = c(10, 10^4),
+                breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  xlab(expression(bold("Observed concentration " *Sigma*"PCB (pg/L)"))) +
+  ylab(expression(bold("Predicted lme concentration " *Sigma*"PCB (pg/L)"))) +
+  geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
+  geom_abline(intercept = 0.30103, slope = 1, col = "blue",
+              linewidth = 0.7) + # 1:2 line (factor of 2)
+  geom_abline(intercept = -0.30103, slope = 1, col = "blue",
+              linewidth = 0.7) + # 2:1 line (factor of 2)
+  theme_bw() +
+  theme(aspect.ratio = 15/15) +
+  annotation_logticks(sides = "bl")
+
+# Estimate a factor of 2 between observations and predictions
+# Create a data frame with observed and predicted values
+compare_df.2 <- data.frame(observed = test_data$tPCB,
+                           predicted = 10^predictions.2)
+
+# Estimate a factor of 2 between observations and predictions
+compare_df.2$factor2 <- compare_df.2$observed/compare_df.2$predicted
+
+# Calculate the percentage of observations within the factor of 2
+factor2_percentage.2 <- nrow(compare_df.2[compare_df.2$factor2 > 0.5 & compare_df.2$factor2 < 2, ])/nrow(compare_df.2)*100
+
+# Model (3)
+# Only flow.4
+# Fit the Model (3)
+rf_model.3 <- randomForest(log10(tPCB) ~ time + site.code + season + flow.4,
+                           data = train_data)
+
+# Make Predictions
+predictions.3 <- predict(rf_model.3, newdata = test_data)
+
+# Evaluate Model Performance
+mse.3 <- mean((predictions.3 - log10(test_data$tPCB))^2)
+rmse.3 <- sqrt(mse.3)
+mae.3 <- mean(abs(predictions.3 - log10(test_data$tPCB)))
+r_squared.3 <- 1 - (sum((log10(test_data$tPCB) - predictions.3)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
+
+# Feature Importance
+importance.3 <- importance(rf_model.3)
+# Plot features
+barplot(importance.3[, 1], names.arg = rownames(importance.3),
+        main = "Feature Importance", las = 2, cex.names = 0.7)
+
+# Create a data frame for plotting
+plot_data.3 <- data.frame(
+  Actual = log10(test_data$tPCB),
+  Predicted = predictions.3
+)
+
+# Create the scatter plot using ggplot2
+ggplot(plot_data.3, aes(x = 10^(Actual), y = 10^(Predicted))) +
+  geom_point(shape = 21, size = 3, fill = "white") +
+  scale_y_log10(limits = c(10, 10^5),
+                breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  scale_x_log10(limits = c(10, 10^5),
+                breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  xlab(expression(bold("Observed concentration " *Sigma*"PCB (pg/L)"))) +
+  ylab(expression(bold("Predicted lme concentration " *Sigma*"PCB (pg/L)"))) +
+  geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
+  geom_abline(intercept = 0.30103, slope = 1, col = "blue",
+              linewidth = 0.7) + # 1:2 line (factor of 2)
+  geom_abline(intercept = -0.30103, slope = 1, col = "blue",
+              linewidth = 0.7) + # 2:1 line (factor of 2)
+  theme_bw() +
+  theme(aspect.ratio = 15/15) +
+  annotation_logticks(sides = "bl")
+
+# Estimate a factor of 2 between observations and predictions
+# Create a data frame with observed and predicted values
+compare_df.3 <- data.frame(observed = test_data$tPCB,
+                           predicted = 10^predictions.3)
+
+# Estimate a factor of 2 between observations and predictions
+compare_df.3$factor2 <- compare_df.3$observed/compare_df.3$predicted
+
+# Calculate the percentage of observations within the factor of 2
+factor2_percentage.3 <- nrow(compare_df.3[compare_df.3$factor2 > 0.5 & compare_df.3$factor2 < 2, ])/nrow(compare_df.3)*100
