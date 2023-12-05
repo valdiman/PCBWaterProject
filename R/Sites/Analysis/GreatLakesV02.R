b962733b65e04ca6c49427ad43df8a20ffd2a726
martie <andres-martinez@uiowa.edu>
2023-12-05 17:27:29 -0600

    added the individual PCB analysis.

--- R/Sites/Analysis/GreatLakesV02.R
+++ R/Sites/Analysis/GreatLakesV02.R
@@ -45,9 +45,6 @@ wdc <- read.csv("Data/WaterDataCongenerAroclor09072023.csv")
 # Select LMMB and Great Lakes data ---------------------------------------------------
 grl <- wdc[str_detect(wdc$LocationName, 'Lake Michigan Mass Balance|Great Lakes'), ]
 
-# Just get lake data, remove data from tributaries
-grl <- grl[!grepl("^Tributary", grl$SiteName), ]
-
 # Data preparation --------------------------------------------------------
 {
   # Change date format
@@ -69,72 +66,6 @@ grl <- grl[!grepl("^Tributary", grl$SiteName), ]
                           "tPCB", "time", "site.code", "season")
 }
 
-# Random Forest Model -----------------------------------------------------
-# Train-Test Split
-set.seed(123)
-train_indices <- sample(1:nrow(grl.tpcb), 0.8 * nrow(grl.tpcb))
-train_data <- grl.tpcb[train_indices, ]
-test_data <- grl.tpcb[-train_indices, ]
-
-# Fit the Model (1)
-rf_model.1 <- randomForest(log10(tPCB) ~ time + site.code + season,
-                           data = train_data)
-
-# Make Predictions
-predictions.1 <- predict(rf_model.1, newdata = test_data)
-
-# Evaluate Model Performance
-mse.1 <- mean((predictions.1 - log10(test_data$tPCB))^2)
-rmse.1 <- sqrt(mse.1)
-mae.1 <- mean(abs(predictions.1 - log10(test_data$tPCB)))
-r_squared.1 <- 1 - (sum((log10(test_data$tPCB) - predictions.1)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
-
-# Feature Importance
-importance.1 <- importance(rf_model.1)
-# Plot features
-barplot(importance.1[, 1], names.arg = rownames(importance.1),
-        main = "Feature Importance", las = 2, cex.names = 0.7)
-
-# Create a data frame for plotting
-plot_data.1 <- data.frame(
-  Actual = log10(test_data$tPCB),
-  Predicted = predictions.1
-)
-
-# Create the scatter plot using ggplot2
-ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
-  geom_point(shape = 21, size = 3, fill = "white") +
-  scale_y_log10(limits = c(1, 10^5),
-                breaks = trans_breaks("log10", function(x) 10^x),
-                labels = trans_format("log10", math_format(10^.x))) +
-  scale_x_log10(limits = c(1, 10^5),
-                breaks = trans_breaks("log10", function(x) 10^x),
-                labels = trans_format("log10", math_format(10^.x))) +
-  xlab(expression(bold("Observed concentration " *Sigma*"PCB (pg/L)"))) +
-  ylab(expression(bold("Predicted lme concentration " *Sigma*"PCB (pg/L)"))) +
-  geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
-  geom_abline(intercept = 0.30103, slope = 1, col = "blue",
-              linewidth = 0.7) + # 1:2 line (factor of 2)
-  geom_abline(intercept = -0.30103, slope = 1, col = "blue",
-              linewidth = 0.7) + # 2:1 line (factor of 2)
-  theme_bw() +
-  theme(aspect.ratio = 15/15) +
-  annotation_logticks(sides = "bl")
-
-# Estimate a factor of 2 between observations and predictions
-# Create a data frame with observed and predicted values
-compare_df.1 <- data.frame(observed = test_data$tPCB,
-                           predicted = 10^predictions.1)
-
-# Estimate a factor of 2 between observations and predictions
-compare_df.1$factor2 <- compare_df.1$observed/compare_df.1$predicted
-
-# Calculate the percentage of observations within the factor of 2
-factor2_percentage.1 <- nrow(compare_df.1[compare_df.1$factor2 > 0.5 & compare_df.1$factor2 < 2, ])/nrow(compare_df.1)*100
-
-# Only samples from lake Michigan
-grl.tpcb.1 <- subset(grl.tpcb, grepl("LMM", SiteID))
-
 # Read water temperature
 wt93 <- read.csv("Output/Data/Sites/csv/GreatLakes/WT1993.csv")
 wt94 <- read.csv("Output/Data/Sites/csv/GreatLakes/WT1994.csv")
@@ -147,49 +78,80 @@ wt <- rbind(wt93, wt94, wt95)
 wt$Date <- as.Date(wt$Date)
 
 # Add water temperature to grl.tpcb
-grl.tpcb.1$temp <- wt$WTMP_K[match(grl.tpcb.1$date, wt$Date)]
+grl.tpcb$temp <- wt$WTMP_K[match(grl.tpcb$date, wt$Date)]
 
 # Remove samples with temp = NA
-grl.tpcb.1 <- na.omit(grl.tpcb.1)
+grl.tpcb <- na.omit(grl.tpcb)
 
+# Random Forest Model -----------------------------------------------------
 # Train-Test Split
 set.seed(123)
-train_indices <- sample(1:nrow(grl.tpcb.1), 0.8 * nrow(grl.tpcb.1))
-train_data <- grl.tpcb.1[train_indices, ]
-test_data <- grl.tpcb.1[-train_indices, ]
+train_indices <- sample(1:nrow(grl.tpcb), 0.8 * nrow(grl.tpcb))
+train_data <- grl.tpcb[train_indices, ]
+test_data <- grl.tpcb[-train_indices, ]
 
 # Fit the Model (1)
-rf_model.2 <- randomForest(log10(tPCB) ~ time + temp + site.code + season,
+rf_model.1 <- randomForest(log10(tPCB) ~ time + site.code + season + temp,
                            data = train_data)
 
 # Make Predictions
-predictions.2 <- predict(rf_model.2, newdata = test_data)
+predictions.1 <- predict(rf_model.1, newdata = test_data)
 
 # Evaluate Model Performance
-mse.2 <- mean((predictions.2 - log10(test_data$tPCB))^2)
-rmse.2 <- sqrt(mse.2)
-mae.2 <- mean(abs(predictions.2 - log10(test_data$tPCB)))
-r_squared.2 <- 1 - (sum((log10(test_data$tPCB) - predictions.2)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
+mse.1 <- mean((predictions.1 - log10(test_data$tPCB))^2)
+rmse.1 <- sqrt(mse.1)
+r_squared.1 <- 1 - (sum((log10(test_data$tPCB) - predictions.1)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
+
+# Estimate a factor of 2 between observations and predictions
+# Create a data frame with observed and predicted values
+compare_df.1 <- data.frame(observed = test_data$tPCB,
+                           predicted = 10^predictions.1)
+
+# Estimate a factor of 2 between observations and predictions
+compare_df.1$factor2 <- compare_df.1$observed/compare_df.1$predicted
+
+# Calculate the percentage of observations within the factor of 2
+factor2_percentage.1 <- nrow(compare_df.1[compare_df.1$factor2 > 0.5 & compare_df.1$factor2 < 2, ])/nrow(compare_df.1)*100
+
+# Create the data frame directly
+performance_df <- data.frame(Heading = c("RMSE", "R2", "Factor2"),
+                             Value = c(rmse.1, r_squared.1,
+                                       factor2_percentage.1))
+
+# Remove unnecessary columns
+performance_df <- performance_df[, !(names(performance_df) %in% c("V1", "V2", "V3"))]
+
+# Print the modified data frame
+print(performance_df)
+
+# Export results
+write.csv(performance_df,
+          file = "Output/Data/Sites/csv/GreatLakes/GreatLakesRFPerformancetPCB.csv")
 
 # Feature Importance
-importance.2 <- importance(rf_model.2)
+importance.1 <- importance(rf_model.1)
 # Plot features
-barplot(importance.2[, 1], names.arg = rownames(importance.2),
+barplot(importance.1[, 1], names.arg = rownames(importance.1),
         main = "Feature Importance", las = 2, cex.names = 0.7)
 
 # Create a data frame for plotting
-plot_data.2 <- data.frame(
+plot_data.1 <- data.frame(
+  Location = rep("Great Lakes", nrow(test_data)),
   Actual = log10(test_data$tPCB),
-  Predicted = predictions.2
+  Predicted = predictions.1
 )
 
-# Create the scatter plot using ggplot2
-ggplot(plot_data.2, aes(x = 10^(Actual), y = 10^(Predicted))) +
+# Export results
+write.csv(plot_data.1,
+          file = "Output/Data/Sites/csv/GreatLakes/GreatLakesRFObsPredtPCB.csv")
+
+# Create the scatter plot
+plotRF <- ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
   geom_point(shape = 21, size = 3, fill = "white") +
-  scale_y_log10(limits = c(10, 10^4),
+  scale_y_log10(limits = c(10, 10^5),
                 breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x))) +
-  scale_x_log10(limits = c(10, 10^4),
+  scale_x_log10(limits = c(10, 10^5),
                 breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x))) +
   xlab(expression(bold("Observed concentration " *Sigma*"PCB (pg/L)"))) +
@@ -203,15 +165,155 @@ ggplot(plot_data.2, aes(x = 10^(Actual), y = 10^(Predicted))) +
   theme(aspect.ratio = 15/15) +
   annotation_logticks(sides = "bl")
 
-# Estimate a factor of 2 between observations and predictions
-# Create a data frame with observed and predicted values
-compare_df.2 <- data.frame(observed = test_data$tPCB,
-                           predicted = 10^predictions.2)
+# Print the plot
+print(plotRF)
 
-# Estimate a factor of 2 between observations and predictions
-compare_df.2$factor2 <- compare_df.2$observed/compare_df.2$predicted
+# Save plot in folder
+ggsave("Output/Plots/Sites/ObsPred/GreatLakes/GreatLakesRFtPCBV01.png",
+       plot = plotRF, width = 6, height = 5, dpi = 500)
 
-# Calculate the percentage of observations within the factor of 2
-factor2_percentage.2 <- nrow(compare_df.2[compare_df.2$factor2 > 0.5 & compare_df.2$factor2 < 2, ])/nrow(compare_df.2)*100
+# Individual PCB Analysis -------------------------------------------------
+# Prepare data.frame
+{
+  grl.pcb <- subset(grl, select = -c(SampleID:AroclorCongener))
+  # Remove Aroclor data
+  grl.pcb <- subset(grl.pcb, select = -c(A1016:tPCB))
+  # Log10 individual PCBs 
+  grl.pcb <- log10(grl.pcb)
+  # Replace -inf to NA
+  grl.pcb <- do.call(data.frame,
+                     lapply(grl.pcb,
+                            function(x) replace(x, is.infinite(x), NA)))
+  # Remove individual PCB that have 30% or less NA values
+  grl.pcb.1 <- grl.pcb[,
+                       -which(colSums(is.na(grl.pcb))/nrow(grl.pcb) > 0.7)]
+  
+  # Create individual code for each site sampled
+  site.numb <- grl$SiteID %>% as.factor() %>% as.numeric
+  # Change date format
+  SampleDate <- as.Date(grl$SampleDate, format = "%m/%d/%y")
+  # Calculate sampling time
+  time.day <- data.frame(as.Date(SampleDate) - min(as.Date(SampleDate)))
+  # Change name time.day to time
+  colnames(time.day) <- "time"
+  # Include season
+  yq.s <- as.yearqtr(as.yearmon(grl$SampleDate, "%m/%d/%Y") + 1/12)
+  season.s <- factor(format(yq.s, "%q"), levels = 1:4,
+                     labels = c("0", "S-1", "S-2", "S-3")) # winter, spring, summer, fall
+  # Add date and time to grl.pcb.1
+  grl.pcb.1 <- cbind(grl.pcb.1, SampleDate, data.frame(time.day),
+                     site.numb, season.s)
+  # Add water temperature to grl.pcb.1
+  grl.pcb.1$temp <- wt$WTMP_K[match(grl.pcb.1$SampleDate, wt$Date)]
+  # Remove samples with temperature = NA
+  grl.pcb.2 <- grl.pcb.1[!is.na(grl.pcb.1$temp), ]
+}
 
+# Set the seed for reproducibility
+set.seed(123)
 
+# Find the numeric columns (columns starting with "PCB")
+pcb_numeric_columns <- grep("^PCB", colnames(grl.pcb.2), value = TRUE)
+
+# Find the corresponding character columns
+char_columns <- setdiff(colnames(grl.pcb.2), pcb_numeric_columns)
+
+# Initialize the results matrix
+rf_results <- data.frame(
+  Congener = pcb_numeric_columns,
+  RMSE = rep(NA, length(pcb_numeric_columns)),
+  R_squared = rep(NA, length(pcb_numeric_columns)),
+  Factor2_Percentage = rep(NA, length(pcb_numeric_columns))
+)
+
+# Create an empty data frame to store all predicted and actual data
+all_results <- data.frame()
+
+# Iterate over each numeric column
+for (i in seq_along(pcb_numeric_columns)) {
+  # Combine numeric and character data
+  combined_data <- cbind(grl.pcb.2[, pcb_numeric_columns[i],
+                                   drop = FALSE], grl.pcb.2[, char_columns])
+  
+  # Exclude rows with missing values
+  combined_data <- na.omit(combined_data)
+  
+  # Sample indices for training
+  train_indices <- sample(1:nrow(combined_data), 0.8 * nrow(combined_data))
+  
+  # Create separate training and testing sets
+  train_data <- combined_data[train_indices, ]
+  test_data <- combined_data[-train_indices, ]
+  
+  # Remove the SampleDate column from the training data
+  train_data <- train_data[, -which(names(train_data) == "SampleDate")]
+  
+  # Modeling code using randomForest
+  fit <- randomForest(train_data[, 1] ~ ., data = train_data)
+  
+  # Example: Make predictions on the test set
+  predictions <- predict(fit, newdata = test_data)
+  
+  # Calculate mean squared error (mse) for illustration
+  mse <- mean((predictions - test_data[, 1])^2)
+  
+  # Calculate R-squared
+  r_squared <- 1 - (sum((test_data[, 1] - predictions)^2) / sum((test_data[, 1] - mean(test_data[, 1]))^2))
+  
+  # Calculate factor2_percentage within the loop
+  compare_df <- data.frame(
+    observed = test_data[, 1],
+    predicted = predictions
+  )
+  compare_df$factor2 <- compare_df$observed / compare_df$predicted
+  factor2_percentage <- sum(compare_df$factor2 > 0.5 & compare_df$factor2 < 2) / nrow(compare_df) * 100
+  
+  # Store the results in the matrix
+  rf_results[i, 2:4] <- c(mse, r_squared, factor2_percentage)
+  
+  # Create a data frame for each column's results
+  col_results <- data.frame(
+    Location = rep("Great Lakes", length(test_data[, 1])),
+    Congener = rep(pcb_numeric_columns[i], length(test_data[, 1])),
+    Actual = test_data[, 1],
+    Predicted = predictions
+  )
+  
+  # Bind the data frame to the overall results
+  all_results <- rbind(all_results, col_results)
+}
+
+# Export results
+write.csv(rf_results,
+          file = "Output/Data/Sites/csv/GreatLakes/GreatLakesRFPerformancePCB.csv",
+          row.names = FALSE)
+
+# Export combined results
+write.csv(all_results,
+          file = "Output/Data/Sites/csv/GreatLakes/GreatLakesRFObsPredPCB.csv",
+          row.names = FALSE)
+
+# Plot
+plotRFPCBi <- ggplot(all_results, aes(x = 10^(Actual), y = 10^(Predicted))) +
+  geom_point(shape = 21, size = 3, fill = "white") +
+  scale_y_log10(limits = c(0.01, 10^4),
+                breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  scale_x_log10(limits = c(0.01, 10^4),
+                breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  xlab(expression(bold("Observed concentration PCBi (pg/L)"))) +
+  ylab(expression(bold("Predicted lme concentration PCBi (pg/L)"))) +
+  geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
+  geom_abline(intercept = 0.30103, slope = 1, col = "blue", linewidth = 0.7) + # 1:2 line (factor of 2)
+  geom_abline(intercept = -0.30103, slope = 1, col = "blue", linewidth = 0.7) + # 2:1 line (factor of 2)
+  theme_bw() +
+  theme(aspect.ratio = 15/15) +
+  annotation_logticks(sides = "bl")
+
+# Print the plot
+print(plotRFPCBi)
+
+# Save plot in folder
+ggsave("Output/Plots/Sites/ObsPred/GreatLakes/GreatLakesRFPCBV01.png",
+       plot = plotRFPCBi, width = 6, height = 5, dpi = 500)
