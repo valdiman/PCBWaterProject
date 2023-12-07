4cf0067fd6a4baf22a868a42f1aa6dbdcdaba3a7
martie <andres-martinez@uiowa.edu>
2023-12-06 21:32:44 -0600

    Updated version.

--- R/Figures/RFPredicObserPlotPCB.R
+++ R/Figures/RFPredicObserPlotPCB.R
@@ -13,62 +13,59 @@ install.packages("RColorBrewer")
 
 # Read generated data
 {
-  # 21 Mich
-  mic <- read.csv("Output/Data/Sites/csv/21Mich/21MichObsPredPCB.csv")
-  mic <- mic[, -c(1:2)]
+  # Anacostia River
   # Bannister Federal Complex
-  bfc <- read.csv("Output/Data/Sites/csv/BannisterFedComplex/BannisterFedComplexObsPredPCB.csv")
-  bfc <- bfc[, -c(1:2)]
+  #bfc <- read.csv("Output/Data/Sites/csv/BannisterFedComplex/BannisterFedComplexObsPredtPCB.csv")
+  #bfc <- bfc[, -1]
   # Chesapeake Bay data
-  che <- read.csv("Output/Data/Sites/csv/ChesapeakeBay/ChesapeakeObsPredPCB.csv")
-  che <- che[, -c(1:2)]
+  #che <- read.csv("Output/Data/Sites/csv/ChesapeakeBay/ChesapeakeObsPredtPCB.csv")
+  #che <- che[, -1]
   # Fox River data
-  fox <- read.csv("Output/Data/Sites/csv/FoxRiver/FoxRiverObsPredPCB.csv")
-  fox <- fox[, -c(1:2)]
+  fox <- read.csv("Output/Data/Sites/csv/FoxRiver/FoxRiverRFObsPredPCB.csv")
+  fox <- fox[, -2]
   # Great Lakes
-  grl <- read.csv("Output/Data/Sites/csv/GreatLakes/GreatLakesObsPredPCB.csv")
-  grl <- grl[, -1]
+  grl <- read.csv("Output/Data/Sites/csv/GreatLakes/GreatLakesRFObsPredPCB.csv")
+  grl <- grl[, -2]
   # Hudson River
-  hud <- read.csv("Output/Data/Sites/csv/HudsonRiver/HudsonRiverObsPredPCB.csv")
-  hud <- hud[, -c(1:2)]
+  hud <- read.csv("Output/Data/Sites/csv/HudsonRiver/HudsonRiverRFObsPredPCB.csv")
+  hud <- hud[, -2]
   # Lake Washington
-  lwa <- read.csv("Output/Data/Sites/csv/LakeWashington/LakeWashingtonObsPredPCB.csv")
-  lwa <- lwa[, -c(1:2)]
-  # Tributaries to Lake Michigan
-  glt <- read.csv("Output/Data/Sites/csv/GreatLakes/TributariesObsPredPCB.csv")
-  glt <- glt[, -c(1:2)]
+  #lwa <- read.csv("Output/Data/Sites/csv/LakeWashington/LakeWashingtonObsPredtPCB.csv")
+  #lwa <- lwa[, -1]
   # New Bedford Harbor data
-  nbh <- read.csv("Output/Data/Sites/csv/NewBedfordHarbor/NBHObsPredPCB.csv")
-  nbh <- nbh[, -c(1:2)]
+  nbh <- read.csv("Output/Data/Sites/csv/NewBedfordHarbor/NBHRFObsPredPCB.csv")
+  nbh <- nbh[, -2]
   # Passaic River
-  pas <- read.csv("Output/Data/Sites/csv/PassaicRiver/PassaicObsPredPCB.csv")
-  pas <- pas[,-c(1:2)]
+  pas <- read.csv("Output/Data/Sites/csv/PassaicRiver/PassaicRiverRFObsPredPCB.csv")
+  pas <- pas[, -2]
   # Portland Harbord data
-  por <- read.csv("Output/Data/Sites/csv/PortlandHarbor/PortlandHarborObsPredPCB.csv")
-  por <- por[, -c(1:2)]
+  por <- read.csv("Output/Data/Sites/csv/PortlandHarbor/PortlandHarborRFObsPredPCB.csv")
+  por <- por[, -2]
+  # Richardson Hill Road Landfill
+  #rhr <- read.csv("Output/Data/Sites/csv/Richardson/RichardsonObsPredtPCB.csv")
+  #rhr <- rhr[, -1]
   # Spokane River data
-  spo <- read.csv("Output/Data/Sites/csv/SpokaneRiver/SpokaneRiverObsPredPCB.csv")
-  spo <- spo[, -c(1:2)]
+  spo <- read.csv("Output/Data/Sites/csv/SpokaneRiver/SpokaneRiverRFObsPredPCB.csv")
+  spo <- spo[, -2]
   # Combine the data frames
-  combined_data <- rbind(mic, bfc, che, fox, grl, glt, hud, lwa, nbh, pas,
-                         por, spo)
+  combined_data <- rbind(fox, grl, hud, nbh, pas, por, spo)
 }
 
 # Create a custom color palette with 12 different colors
-n_colors <- 12
-custom_palette <- colorRampPalette(brewer.pal(9, "Set1"))(n_colors)
+n_colors <- 7
+custom_palette <- colorRampPalette(brewer.pal(7, "Set1"))(n_colors)
 
-# Add a new column with the number of rows for each LocationName
-combined_data$Location <- paste0(combined_data$LocationName,
-                                 " (n=", ave(combined_data$LocationName,
-                                             combined_data$LocationName,
+# Add a new column with the number of rows for each Location
+combined_data$Location <- paste0(combined_data$Location,
+                                 " (n=", ave(combined_data$Location,
+                                             combined_data$Location,
                                              FUN = length), ")")                                                                         
 
 # Plot prediction vs. observations, 1:1 line
 CombinePredObsPlot <- ggplot(combined_data,
-                             aes(x = 10^(observed), y = 10^(predicted),
+                             aes(x = 10^(Actual), y = 10^(Predicted),
                                  fill = Location)) +  # Use the new column for legend labels
-  geom_point(shape = 21, size = 1.2, alpha = 0.5) +
+  geom_point(shape = 21, size = 3, alpha = 0.5) +
   scale_y_log10(limits = c(0.001, 10^7), breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x))) +
   scale_x_log10(limits = c(0.001, 10^7), breaks = trans_breaks("log10", function(x) 10^x),
@@ -93,6 +90,6 @@ CombinePredObsPlot <- ggplot(combined_data,
 print(CombinePredObsPlot)
 
 # Save plot
-ggsave("Output/Figures/Sites/CombineObsPredPCBi.png",
+ggsave("Output/Figures/Sites/CombineRFObsPredPCBi.png",
        plot = CombinePredObsPlot, width = 18, height = 8, dpi = 500)
 
