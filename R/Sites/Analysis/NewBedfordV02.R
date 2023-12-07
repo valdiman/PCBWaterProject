3c5c4aaa819053e694ffe9df9cd9df4e21a10f61
martie <andres-martinez@uiowa.edu>
2023-12-06 21:29:44 -0600

    Included individual PCB analysis using random forest.

--- R/Sites/Analysis/NewBedfordV02.R
+++ R/Sites/Analysis/NewBedfordV02.R
@@ -63,8 +63,6 @@ nbh <- wdc[str_detect(wdc$LocationName, 'New Bedford'),]
   nbh$SampleDate <- as.Date(nbh$SampleDate, format = "%m/%d/%y")
   # Calculate sampling time
   time.day <- data.frame(as.Date(nbh$SampleDate) - min(as.Date(nbh$SampleDate)))
-  # Create individual code for each site sampled
-  site.numb <- nbh$SiteID %>% as.factor() %>% as.numeric
   # Include season
   yq.s <- as.yearqtr(as.yearmon(nbh$SampleDate, "%m/%d/%Y") + 1/12)
   season.s <- factor(format(yq.s, "%q"), levels = 1:4,
@@ -72,41 +70,37 @@ nbh <- wdc[str_detect(wdc$LocationName, 'New Bedford'),]
   # Create data frame
   nbh.tpcb <- cbind(factor(nbh$SiteID), nbh$SampleDate,
                     nbh$Latitude, nbh$Longitude, as.matrix(nbh$tPCB),
-                    data.frame(time.day), site.numb, season.s)
+                    data.frame(time.day), season.s)
   # Add column names
   colnames(nbh.tpcb) <- c("SiteID", "date", "Latitude", "Longitude",
-                          "tPCB", "time", "site.code", "season")
+                          "tPCB", "time", "season")
 }
 
-# Source locations
-source1 <- c(Latitude = 41.675042, Longitude = -70.916295)  # Former Aerovox Site
-
-# Create sf points
-source1_sf <- st_point(c(source1["Latitude"], source1["Longitude"]))
-
-# Initialize a list to store point geometries
-point_list <- list()
-
-# Loop to create point geometries
-for (i in 1:nrow(nbh.tpcb)) {
-  point <- st_point(c(nbh.tpcb[i, "Latitude"], nbh.tpcb[i, "Longitude"]))
-  point_list[[i]] <- point
+# Include distance to sources
+{
+  # Source locations
+  source1 <- c(Latitude = 41.675042, Longitude = -70.916295)  # Former Aerovox Site
+  # Create sf points
+  source1_sf <- st_point(c(source1["Latitude"], source1["Longitude"]))
+  # Initialize a list to store point geometries
+  point_list <- list()
+  # Loop to create point geometries
+  for (i in 1:nrow(nbh.tpcb)) {
+    point <- st_point(c(nbh.tpcb[i, "Latitude"], nbh.tpcb[i, "Longitude"]))
+    point_list[[i]] <- point
+  }
+  # Create an sf object from the list of point geometries
+  nbh.tpcb_sf <- st_sf(geometry = st_sfc(point_list), crs = 4326)
+  # Ensure that both objects have the same CRS
+  st_crs(nbh.tpcb_sf) <- st_crs(source1_sf)
+  # Calculate distances
+  distances <- st_distance(nbh.tpcb_sf, source1_sf) * 100
+  # Add distances to nbh.tpcb data.frame
+  nbh.tpcb$Distance_to_source1 <- distances
 }
-
-# Create an sf object from the list of point geometries
-nbh.tpcb_sf <- st_sf(geometry = st_sfc(point_list), crs = 4326)
-
-# Ensure that both objects have the same CRS
-st_crs(nbh.tpcb_sf) <- st_crs(source1_sf)
-
-# Calculate distances
-distances <- st_distance(nbh.tpcb_sf, source1_sf) * 100
-
-# Add distances to hud.tpcb data.frame
-nbh.tpcb$Distance_to_source1 <- distances
 
 # Random Forest Model -----------------------------------------------------
-# Using all the data, hud.tpcb.1
+# Using all the data, nbh.tpcb.1
 # Train-Test Split
 set.seed(123)
 train_indices <- sample(1:nrow(nbh.tpcb), 0.8 * nrow(nbh.tpcb))
@@ -114,17 +108,42 @@ train_data <- nbh.tpcb[train_indices, ]
 test_data <- nbh.tpcb[-train_indices, ]
 
 # Fit the Model (1)
-rf_model.1 <- randomForest(log10(tPCB) ~ time + site.code + season +
+rf_model.1 <- randomForest(log10(tPCB) ~ time + SiteID + season +
                              Distance_to_source1, data = train_data)
 
 # Make Predictions
 predictions.1 <- predict(rf_model.1, newdata = test_data)
 
 # Evaluate Model Performance
-mse.1 <- mean((predictions.1 - log10(test_data$tPCB))^2)
-rmse.1 <- sqrt(mse.1)
-mae.1 <- mean(abs(predictions.1 - log10(test_data$tPCB)))
-r_squared.1 <- 1 - (sum((log10(test_data$tPCB) - predictions.1)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
+mse <- mean((predictions.1 - log10(test_data$tPCB))^2)
+rmse <- sqrt(mse)
+r_squared <- 1 - (sum((log10(test_data$tPCB) - predictions.1)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
+
+# Estimate a factor of 2 between observations and predictions
+# Create a data frame with observed and predicted values
+compare_df <- data.frame(observed = test_data$tPCB,
+                         predicted = 10^predictions.1)
+
+# Estimate a factor of 2 between observations and predictions
+compare_df$factor2 <- compare_df$observed/compare_df$predicted
+
+# Calculate the percentage of observations within the factor of 2
+factor2_percentage <- nrow(compare_df[compare_df$factor2 > 0.5 & compare_df$factor2 < 2, ])/nrow(compare_df)*100
+
+# Create the data frame directly
+performance_df <- data.frame(Heading = c("RMSE", "R2", "Factor2"),
+                             Value = c(rmse, r_squared,
+                                       factor2_percentage))
+
+# Remove unnecessary columns
+performance_df <- performance_df[, !(names(performance_df) %in% c("V1", "V2", "V3"))]
+
+# Print the modified data frame
+print(performance_df)
+
+# Export results
+write.csv(performance_df,
+          file = "Output/Data/Sites/csv/NewBedfordHarbor/NBHRFPerformancetPCB.csv")
 
 # Feature Importance
 importance.1 <- importance(rf_model.1)
@@ -134,12 +153,17 @@ barplot(importance.1[, 1], names.arg = rownames(importance.1),
 
 # Create a data frame for plotting
 plot_data.1 <- data.frame(
+  Location = rep("New Bedford Harbor", nrow(test_data)),
   Actual = log10(test_data$tPCB),
   Predicted = predictions.1
 )
 
+# Export results
+write.csv(plot_data.1,
+          file = "Output/Data/Sites/csv/NewBedfordHarbor/NBHRFObsPredtPCB.csv")
+
 # Create the scatter plot using ggplot2
-ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
+plotRF <- ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
   geom_point(shape = 21, size = 3, fill = "white") +
   scale_y_log10(limits = c(10^2, 10^7),
                 breaks = trans_breaks("log10", function(x) 10^x),
@@ -158,16 +182,170 @@ ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
   theme(aspect.ratio = 15/15) +
   annotation_logticks(sides = "bl")
 
-# Estimate a factor of 2 between observations and predictions
-# Create a data frame with observed and predicted values
-compare_df <- data.frame(observed = test_data$tPCB,
-                         predicted = 10^predictions.1)
+# Print the plot
+print(plotRF)
 
-# Estimate a factor of 2 between observations and predictions
-compare_df$factor2 <- compare_df$observed/compare_df$predicted
+# Save plot in folder
+ggsave("Output/Plots/Sites/ObsPred/NewBedfordHarbor/NBHRFtPCBV01.png",
+       plot = plotRF, width = 6, height = 5, dpi = 500)
 
-# Calculate the percentage of observations within the factor of 2
-factor2_percentage <- nrow(compare_df[compare_df$factor2 > 0.5 & compare_df$factor2 < 2, ])/nrow(compare_df)*100
+# Individual PCB Analysis -------------------------------------------------
+# Prepare data.frame
+{
+  # Remove metadata
+  nbh.pcb <- subset(nbh, select = -c(SampleID:AroclorCongener))
+  # Remove Aroclor data
+  nbh.pcb <- subset(nbh.pcb, select = -c(A1016:tPCB))
+  # Log10 individual PCBs 
+  nbh.pcb <- log10(nbh.pcb)
+  # Replace -inf to NA
+  nbh.pcb <- do.call(data.frame,
+                     lapply(nbh.pcb,
+                            function(x) replace(x, is.infinite(x), NA)))
+  # Remove individual PCB that have 30% or less NA values
+  nbh.pcb.1 <- nbh.pcb[,
+                       -which(colSums(is.na(nbh.pcb))/nrow(nbh.pcb) > 0.7)]
+  # Add site ID
+  SiteID <- factor(nbh$SiteID)
+  # Add coordinates
+  Latitude <- nbh$Latitude
+  Longitude <- nbh$Longitude
+  # Change date format
+  SampleDate <- as.Date(nbh$SampleDate, format = "%m/%d/%y")
+  # Calculate sampling time
+  time.day <- data.frame(as.Date(SampleDate) - min(as.Date(SampleDate)))
+  # Change name time.day to time
+  colnames(time.day) <- "time"
+  # Include season
+  yq.s <- as.yearqtr(as.yearmon(nbh$SampleDate, "%m/%d/%Y") + 1/12)
+  season.s <- factor(format(yq.s, "%q"), levels = 1:4,
+                     labels = c("0", "S-1", "S-2", "S-3")) # winter, spring, summer, fall
+  # Add date and time to nbh.pcb.1
+  nbh.pcb.1 <- cbind(nbh.pcb.1, SiteID, Latitude, Longitude, SampleDate,
+                     data.frame(time.day), season.s)
+  # Initialize a list to store point geometries
+  point_list <- list()
+  # Loop to create point geometries
+  for (i in 1:nrow(nbh.pcb.1)) {
+    point <- st_point(c(nbh.pcb.1[i, "Latitude"], nbh.pcb.1[i, "Longitude"]))
+    point_list[[i]] <- point
+  }
+  # Create an sf object from the list of point geometries
+  nbh.pcb_sf <- st_sf(geometry = st_sfc(point_list), crs = 4326)
+  # Ensure that both objects have the same CRS
+  st_crs(nbh.pcb_sf) <- st_crs(source1_sf)
+  # Calculate distances
+  distances1 <- st_distance(nbh.pcb_sf, source1_sf) * 100
+  # Add distances to nbh.tpcb data.frame
+  nbh.pcb.1$Distance_to_source1 <- distances1
+  # Remove metadata not use in the random forest
+  nbh.pcb.1 <- nbh.pcb.1[, !(names(nbh.pcb.1) %in% c("SampleDate", "Latitude",
+                                                     "Longitude"))]
+}
 
+# Set the seed for reproducibility
+set.seed(123)
 
+# Find the numeric columns (columns starting with "PCB")
+pcb_numeric_columns <- grep("^PCB", colnames(nbh.pcb.1), value = TRUE)
+
+# Find the corresponding character columns
+char_columns <- setdiff(colnames(nbh.pcb.1), pcb_numeric_columns)
+
+# Initialize the results matrix
+rf_results <- data.frame(
+  Congener = pcb_numeric_columns,
+  RMSE = rep(NA, length(pcb_numeric_columns)),
+  R_squared = rep(NA, length(pcb_numeric_columns)),
+  Factor2_Percentage = rep(NA, length(pcb_numeric_columns))
+)
+
+# Create an empty data frame to store all predicted and actual data
+all_results <- data.frame()
+
+# Iterate over each numeric column
+for (i in seq_along(pcb_numeric_columns)) {
+  # Combine numeric and character data
+  combined_data <- cbind(nbh.pcb.1[, pcb_numeric_columns[i],
+                                   drop = FALSE], nbh.pcb.1[, char_columns])
+  
+  # Exclude rows with missing values
+  combined_data <- na.omit(combined_data)
+  
+  # Sample indices for training
+  train_indices <- sample(1:nrow(combined_data), 0.8 * nrow(combined_data))
+  
+  # Create separate training and testing sets
+  train_data <- combined_data[train_indices, ]
+  test_data <- combined_data[-train_indices, ]
+  
+  # Modeling code using randomForest
+  fit <- randomForest(train_data[, 1] ~ ., data = train_data)
+  
+  # Example: Make predictions on the test set
+  predictions <- predict(fit, newdata = test_data)
+  
+  # Calculate mean squared error (mse) for illustration
+  mse <- mean((predictions - test_data[, 1])^2)
+  
+  # Calculate R-squared
+  r_squared <- 1 - (sum((test_data[, 1] - predictions)^2) / sum((test_data[, 1] - mean(test_data[, 1]))^2))
+  
+  # Calculate factor2_percentage within the loop
+  compare_df <- data.frame(
+    observed = test_data[, 1],
+    predicted = predictions
+  )
+  compare_df$factor2 <- compare_df$observed / compare_df$predicted
+  factor2_percentage <- sum(compare_df$factor2 > 0.5 & compare_df$factor2 < 2) / nrow(compare_df) * 100
+  
+  # Store the results in the matrix
+  rf_results[i, 2:4] <- c(mse, r_squared, factor2_percentage)
+  
+  # Create a data frame for each column's results
+  col_results <- data.frame(
+    Location = rep("New Bedford Harbor", length(test_data[, 1])),
+    Congener = rep(pcb_numeric_columns[i], length(test_data[, 1])),
+    Actual = test_data[, 1],
+    Predicted = predictions
+  )
+  
+  # Bind the data frame to the overall results
+  all_results <- rbind(all_results, col_results)
+}
+
+# Export results
+write.csv(rf_results,
+          file = "Output/Data/Sites/csv/NewBedfordHarbor/NBHRFPerformancePCB.csv",
+          row.names = FALSE)
+
+# Export combined results
+write.csv(all_results,
+          file = "Output/Data/Sites/csv/NewBedfordHarbor/NBHRFObsPredPCB.csv",
+          row.names = FALSE)
+
+# Plot
+plotRFPCBi <- ggplot(all_results, aes(x = 10^(Actual), y = 10^(Predicted))) +
+  geom_point(shape = 21, size = 3, fill = "white") +
+  scale_y_log10(limits = c(1, 10^7),
+                breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  scale_x_log10(limits = c(1, 10^7),
+                breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  xlab(expression(bold("Observed concentration PCBi (pg/L)"))) +
+  ylab(expression(bold("Predicted lme concentration PCBi (pg/L)"))) +
+  geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
+  geom_abline(intercept = 0.30103, slope = 1, col = "blue", linewidth = 0.7) + # 1:2 line (factor of 2)
+  geom_abline(intercept = -0.30103, slope = 1, col = "blue", linewidth = 0.7) + # 2:1 line (factor of 2)
+  theme_bw() +
+  theme(aspect.ratio = 15/15) +
+  annotation_logticks(sides = "bl")
+
+# Print the plot
+print(plotRFPCBi)
+
+# Save plot in folder
+ggsave("Output/Plots/Sites/ObsPred/NewBedfordHarbor/NBHRFPCBV01.png",
+       plot = plotRFPCBi, width = 6, height = 5, dpi = 500)
 
