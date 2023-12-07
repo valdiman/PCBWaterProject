4343326dca6d2fec9619de6d63a608adbca4f121
martie <andres-martinez@uiowa.edu>
2023-12-06 21:29:24 -0600

    Included individual PCB analysis using random forest.

--- R/Sites/Analysis/PassaicRiverV02.R
+++ R/Sites/Analysis/PassaicRiverV02.R
@@ -66,32 +66,28 @@ pass <- wdc[str_detect(wdc$LocationName, 'Passaic River'),]
                           "tPCB", "time", "site.code", "season")
 }
 
-# Source locations
-source1 <- c(Latitude = 40.740238, Longitude = -74.133805)  # Former Diamond Alkali
-
-# Create sf points
-source1_sf <- st_point(c(source1["Latitude"], source1["Longitude"]))
-
-# Initialize a list to store point geometries
-point_list <- list()
-
-# Loop to create point geometries
-for (i in 1:nrow(pass.tpcb)) {
-  point <- st_point(c(pass.tpcb[i, "Latitude"], pass.tpcb[i, "Longitude"]))
-  point_list[[i]] <- point
+# Include distance to sources
+{
+  # Source locations
+  source1 <- c(Latitude = 40.740238, Longitude = -74.133805)  # Former Diamond Alkali
+  # Create sf points
+  source1_sf <- st_point(c(source1["Latitude"], source1["Longitude"]))
+  # Initialize a list to store point geometries
+  point_list <- list()
+  # Loop to create point geometries
+  for (i in 1:nrow(pass.tpcb)) {
+    point <- st_point(c(pass.tpcb[i, "Latitude"], pass.tpcb[i, "Longitude"]))
+    point_list[[i]] <- point
+  }
+  # Create an sf object from the list of point geometries
+  pass.tpcb_sf <- st_sf(geometry = st_sfc(point_list), crs = 4326)
+  # Ensure that both objects have the same CRS
+  st_crs(pass.tpcb_sf) <- st_crs(source1_sf)
+  # Calculate distances
+  distances <- st_distance(pass.tpcb_sf, source1_sf) * 100
+  # Add distances to pass.tpcb data.frame
+  pass.tpcb$Distance_to_source1 <- distances
 }
-
-# Create an sf object from the list of point geometries
-pass.tpcb_sf <- st_sf(geometry = st_sfc(point_list), crs = 4326)
-
-# Ensure that both objects have the same CRS
-st_crs(pass.tpcb_sf) <- st_crs(source1_sf)
-
-# Calculate distances
-distances <- st_distance(pass.tpcb_sf, source1_sf) * 100
-
-# Add distances to hud.tpcb data.frame
-pass.tpcb$Distance_to_source1 <- distances
 
 # Include USGS flow and temperature data --------------------------------------------------
 {
@@ -140,63 +136,30 @@ pass.tpcb$Distance_to_source1 <- distances
 pass.tpcb.1 <- subset(pass.tpcb, SiteID != c("WCPCB-PASS022"))
 
 # Random Forest Model -----------------------------------------------------
-# Using all the data, pass.tpcb.1, no flow.3
+# Using flow.1
 # Train-Test Split
 set.seed(123)
 train_indices <- sample(1:nrow(pass.tpcb.1), 0.8 * nrow(pass.tpcb.1))
 train_data <- pass.tpcb.1[train_indices, ]
 test_data <- pass.tpcb.1[-train_indices, ]
 
-# Fit the Model (1)
+# Fit the Model
 rf_model.1 <- randomForest(log10(tPCB) ~ time + site.code + season +
-                             flow.1 + flow.2 + flow.4 + flow.5 +
-                             temp + Distance_to_source1, data = train_data)
+                             flow.1 + temp + Distance_to_source1,
+                           data = train_data)
 
 # Make Predictions
 predictions.1 <- predict(rf_model.1, newdata = test_data)
 
 # Evaluate Model Performance
-mse.1 <- mean((predictions.1 - log10(test_data$tPCB))^2)
-rmse.1 <- sqrt(mse.1)
-mae.1 <- mean(abs(predictions.1 - log10(test_data$tPCB)))
-r_squared.1 <- 1 - (sum((log10(test_data$tPCB) - predictions.1)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
-
-# Feature Importance
-importance.1 <- importance(rf_model.1)
-# Plot features
-barplot(importance.1[, 1], names.arg = rownames(importance.1),
-        main = "Feature Importance", las = 2, cex.names = 0.7)
-
-# Create a data frame for plotting
-plot_data.1 <- data.frame(
-  Actual = log10(test_data$tPCB),
-  Predicted = predictions.1
-)
-
-# Create the scatter plot using ggplot2
-ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
-  geom_point(shape = 21, size = 3, fill = "white") +
-  scale_y_log10(limits = c(10, 10^6),
-                breaks = trans_breaks("log10", function(x) 10^x),
-                labels = trans_format("log10", math_format(10^.x))) +
-  scale_x_log10(limits = c(10, 10^6),
-                breaks = trans_breaks("log10", function(x) 10^x),
-                labels = trans_format("log10", math_format(10^.x))) +
-  xlab(expression(bold("Observed concentration " *Sigma*"PCB (pg/L)"))) +
-  ylab(expression(bold("Predicted lme concentration " *Sigma*"PCB (pg/L)"))) +
-  geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
-  geom_abline(intercept = 0.30103, slope = 1, col = "blue",
-              linewidth = 0.7) + # 1:2 line (factor of 2)
-  geom_abline(intercept = -0.30103, slope = 1, col = "blue",
-              linewidth = 0.7) + # 2:1 line (factor of 2)
-  theme_bw() +
-  theme(aspect.ratio = 15/15) +
-  annotation_logticks(sides = "bl")
+mse <- mean((predictions.1 - log10(test_data$tPCB))^2)
+rmse <- sqrt(mse)
+r_squared <- 1 - (sum((log10(test_data$tPCB) - predictions.1)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
 
 # Estimate a factor of 2 between observations and predictions
 # Create a data frame with observed and predicted values
 compare_df.1 <- data.frame(observed = test_data$tPCB,
-                         predicted = 10^predictions.1)
+                           predicted = 10^predictions.1)
 
 # Estimate a factor of 2 between observations and predictions
 compare_df.1$factor2 <- compare_df.1$observed/compare_df.1$predicted
@@ -204,35 +167,39 @@ compare_df.1$factor2 <- compare_df.1$observed/compare_df.1$predicted
 # Calculate the percentage of observations within the factor of 2
 factor2_percentage.1 <- nrow(compare_df.1[compare_df.1$factor2 > 0.5 & compare_df.1$factor2 < 2, ])/nrow(compare_df.1)*100
 
-# Fit the Model (2)
-# Only one flow, flow.1
-rf_model.2 <- randomForest(log10(tPCB) ~ time + site.code + season +
-                             flow.1 + temp + Distance_to_source1,
-                           data = train_data)
+# Create the data frame directly
+performance_df <- data.frame(Heading = c("RMSE", "R2", "Factor2"),
+                             Value = c(rmse, r_squared,
+                                       factor2_percentage.1))
 
-# Make Predictions
-predictions.2 <- predict(rf_model.2, newdata = test_data)
+# Remove unnecessary columns
+performance_df <- performance_df[, !(names(performance_df) %in% c("V1", "V2", "V3"))]
 
-# Evaluate Model Performance
-mse.2 <- mean((predictions.2 - log10(test_data$tPCB))^2)
-rmse.2 <- sqrt(mse.2)
-mae.2 <- mean(abs(predictions.2 - log10(test_data$tPCB)))
-r_squared.2 <- 1 - (sum((log10(test_data$tPCB) - predictions.2)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
+# Print the modified data frame
+print(performance_df)
+
+# Export results
+write.csv(performance_df,
+          file = "Output/Data/Sites/csv/PassaicRiver/PassaicRiverRFPerformancetPCB.csv")
 
 # Feature Importance
-importance.2 <- importance(rf_model.2)
-# Plot features
-barplot(importance.2[, 1], names.arg = rownames(importance.2),
+importance.1 <- importance(rf_model.1)
+barplot(importance.1[, 1], names.arg = rownames(importance.1),
         main = "Feature Importance", las = 2, cex.names = 0.7)
 
 # Create a data frame for plotting
-plot_data.2 <- data.frame(
+plot_data.1 <- data.frame(
+  Location = rep("Passaic River", nrow(test_data)),
   Actual = log10(test_data$tPCB),
-  Predicted = predictions.2
+  Predicted = predictions.1
 )
 
-# Create the scatter plot using ggplot2
-ggplot(plot_data.2, aes(x = 10^(Actual), y = 10^(Predicted))) +
+# Export results
+write.csv(plot_data.1,
+          file = "Output/Data/Sites/csv/PassaicRiver/PassaicRiverRFObsPredtPCB.csv")
+
+# Create the scatter plot
+plotRF <- ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
   geom_point(shape = 21, size = 3, fill = "white") +
   scale_y_log10(limits = c(10, 10^6),
                 breaks = trans_breaks("log10", function(x) 10^x),
@@ -251,81 +218,195 @@ ggplot(plot_data.2, aes(x = 10^(Actual), y = 10^(Predicted))) +
   theme(aspect.ratio = 15/15) +
   annotation_logticks(sides = "bl")
 
-# Estimate a factor of 2 between observations and predictions
-# Create a data frame with observed and predicted values
-compare_df.2 <- data.frame(observed = test_data$tPCB,
-                           predicted = 10^predictions.2)
+# Print the plot
+print(plotRF)
 
-# Estimate a factor of 2 between observations and predictions
-compare_df.2$factor2 <- compare_df.2$observed/compare_df.2$predicted
+# Save plot in folder
+ggsave("Output/Plots/Sites/ObsPred/PassaicRiver/PassaicRiverRFtPCBV01.png",
+       plot = plotRF, width = 6, height = 5, dpi = 500)
 
-# Calculate the percentage of observations within the factor of 2
-factor2_percentage.2 <- nrow(compare_df.2[compare_df.2$factor2 > 0.5 & compare_df.2$factor2 < 2, ])/nrow(compare_df.2)*100
+# Individual PCB Analysis -------------------------------------------------
+# Prepare data.frame
+{
+  # Remove metadata
+  pass.pcb <- subset(pass, select = -c(SampleID:AroclorCongener))
+  # Remove Aroclor data
+  pass.pcb <- subset(pass.pcb, select = -c(A1016:tPCB))
+  # Log10 individual PCBs 
+  pass.pcb <- log10(pass.pcb)
+  # Replace -inf to NA
+  pass.pcb <- do.call(data.frame,
+                     lapply(pass.pcb,
+                            function(x) replace(x, is.infinite(x), NA)))
+  # Remove individual PCB that have 30% or less NA values
+  pass.pcb.1 <- pass.pcb[,
+                       -which(colSums(is.na(pass.pcb))/nrow(pass.pcb) > 0.7)]
+  # Add site ID
+  SiteID <- factor(pass$SiteID)
+  # Create individual code for each site sampled
+  site.numb <- pass$SiteID %>% as.factor() %>% as.numeric
+  # Add coordinates
+  Latitude <- pass$Latitude
+  Longitude <- pass$Longitude
+  # Change date format
+  SampleDate <- as.Date(pass$SampleDate, format = "%m/%d/%y")
+  # Calculate sampling time
+  time.day <- data.frame(as.Date(SampleDate) - min(as.Date(SampleDate)))
+  # Change name time.day to time
+  colnames(time.day) <- "time"
+  # Include season
+  yq.s <- as.yearqtr(as.yearmon(pass$SampleDate, "%m/%d/%Y") + 1/12)
+  season.s <- factor(format(yq.s, "%q"), levels = 1:4,
+                     labels = c("0", "S-1", "S-2", "S-3")) # winter, spring, summer, fall
+  # Add date and time to pass.pcb.1
+  pass.pcb.1 <- cbind(pass.pcb.1, SiteID, site.numb, Latitude, Longitude, SampleDate,
+                     data.frame(time.day), season.s)
+  # Source location
+  source1 <- c(Latitude = 40.740238, Longitude = -74.133805)  # Former Diamond Alkali
+  # Create sf points
+  source1_sf <- st_point(c(source1["Latitude"], source1["Longitude"]))
+  # Initialize a list to store point geometries
+  point_list <- list()
+  # Loop to create point geometries
+  for (i in 1:nrow(pass.pcb.1)) {
+    point <- st_point(c(pass.pcb.1[i, "Latitude"], pass.pcb.1[i, "Longitude"]))
+    point_list[[i]] <- point
+  }
+  # Create an sf object from the list of point geometries
+  pass.pcb.1_sf <- st_sf(geometry = st_sfc(point_list), crs = 4326)
+  # Ensure that both objects have the same CRS
+  st_crs(pass.pcb.1_sf) <- st_crs(source1_sf)
+  # Calculate distances
+  distances <- st_distance(pass.pcb.1_sf, source1_sf) * 100
+  # Add distances to pass.tpcb data.frame
+  pass.pcb.1$Distance_to_source1 <- distances
+  # Remove site located in the ocean. Possible typo in original coordinates.
+  pass.pcb.1 <- subset(pass.pcb.1, SiteID != c("WCPCB-PASS022"))
+  # Include flow data from USGS station Passaic River
+  sitepassN1 <- "01381900" # No temp
+  sitepassN3 <- "01389005" # No flow
+  # Codes to retrieve data
+  paramflow <- "00060" # discharge, ft3/s
+  paramtemp <- "00010" # water temperature, C
+  # Retrieve USGS data
+  flow.1 <- readNWISdv(sitepassN1, paramflow,
+                       min(pass.pcb.1$SampleDate), max(pass.pcb.1$SampleDate))
+  temp <- readNWISdv(sitepassN3, paramtemp,
+                     min(pass.pcb.1$SampleDate), max(pass.pcb.1$SampleDate))
+  # Add USGS data to pass.tpcb.1, matching dates, conversion to m3/s
+  pass.pcb.1$flow.1 <- 0.03*flow.1$X_00060_00003[match(pass.pcb.1$SampleDate,
+                                                      flow.1$Date)]
+  pass.pcb.1$temp <- 273.15 + temp$X_.from.middle.intake_00010_00003[match(pass.pcb.1$SampleDate,
+                                                                          temp$Date)]
+  # Remove metadata not use in the random forest
+  pass.pcb.1 <- pass.pcb.1[, !(names(pass.pcb.1) %in% c("SiteID", "SampleDate",
+                                                     "Latitude", "Longitude"))]
+}
 
-# Fit the Model (3)
-# Only one flow, flow.3
-# Remove samples with flow.3 = NA
-pass.tpcb.2 <- na.omit(pass.tpcb.1)
-
-# Train-Test Split
+# Set the seed for reproducibility
 set.seed(123)
-train_indices <- sample(1:nrow(pass.tpcb.2), 0.8 * nrow(pass.tpcb.2))
-train_data <- pass.tpcb.2[train_indices, ]
-test_data <- pass.tpcb.2[-train_indices, ]
 
-# Fit the Model (1)
-rf_model.3 <- randomForest(log10(tPCB) ~ time + site.code + season +
-                             flow.3 + temp + Distance_to_source1,
-                           data = train_data)
+# Find the numeric columns (columns starting with "PCB")
+pcb_numeric_columns <- grep("^PCB", colnames(pass.pcb.1), value = TRUE)
 
-# Make Predictions
-predictions.3 <- predict(rf_model.3, newdata = test_data)
+# Find the corresponding character columns
+char_columns <- setdiff(colnames(pass.pcb.1), pcb_numeric_columns)
 
-# Evaluate Model Performance
-mse.3 <- mean((predictions.3 - log10(test_data$tPCB))^2)
-rmse.3 <- sqrt(mse.3)
-mae.3 <- mean(abs(predictions.3 - log10(test_data$tPCB)))
-r_squared.3 <- 1 - (sum((log10(test_data$tPCB) - predictions.3)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
-
-# Feature Importance
-importance.3 <- importance(rf_model.3)
-# Plot features
-barplot(importance.3[, 1], names.arg = rownames(importance.3),
-        main = "Feature Importance", las = 2, cex.names = 0.7)
-
-# Create a data frame for plotting
-plot_data.3 <- data.frame(
-  Actual = log10(test_data$tPCB),
-  Predicted = predictions.3
+# Initialize the results matrix
+rf_results <- data.frame(
+  Congener = pcb_numeric_columns,
+  RMSE = rep(NA, length(pcb_numeric_columns)),
+  R_squared = rep(NA, length(pcb_numeric_columns)),
+  Factor2_Percentage = rep(NA, length(pcb_numeric_columns))
 )
 
-# Create the scatter plot using ggplot2
-ggplot(plot_data.3, aes(x = 10^(Actual), y = 10^(Predicted))) +
+# Create an empty data frame to store all predicted and actual data
+all_results <- data.frame()
+
+# Iterate over each numeric column
+for (i in seq_along(pcb_numeric_columns)) {
+  # Combine numeric and character data
+  combined_data <- cbind(pass.pcb.1[, pcb_numeric_columns[i],
+                                   drop = FALSE], pass.pcb.1[, char_columns])
+  
+  # Exclude rows with missing values
+  combined_data <- na.omit(combined_data)
+  
+  # Sample indices for training
+  train_indices <- sample(1:nrow(combined_data), 0.8 * nrow(combined_data))
+  
+  # Create separate training and testing sets
+  train_data <- combined_data[train_indices, ]
+  test_data <- combined_data[-train_indices, ]
+  
+  # Modeling code using randomForest
+  fit <- randomForest(train_data[, 1] ~ ., data = train_data)
+  
+  # Example: Make predictions on the test set
+  predictions <- predict(fit, newdata = test_data)
+  
+  # Calculate mean squared error (mse) for illustration
+  mse <- mean((predictions - test_data[, 1])^2)
+  
+  # Calculate R-squared
+  r_squared <- 1 - (sum((test_data[, 1] - predictions)^2) / sum((test_data[, 1] - mean(test_data[, 1]))^2))
+  
+  # Calculate factor2_percentage within the loop
+  compare_df <- data.frame(
+    observed = test_data[, 1],
+    predicted = predictions
+  )
+  compare_df$factor2 <- compare_df$observed / compare_df$predicted
+  factor2_percentage <- sum(compare_df$factor2 > 0.5 & compare_df$factor2 < 2) / nrow(compare_df) * 100
+  
+  # Store the results in the matrix
+  rf_results[i, 2:4] <- c(mse, r_squared, factor2_percentage)
+  
+  # Create a data frame for each column's results
+  col_results <- data.frame(
+    Location = rep("Passaic River", length(test_data[, 1])),
+    Congener = rep(pcb_numeric_columns[i], length(test_data[, 1])),
+    Actual = test_data[, 1],
+    Predicted = predictions
+  )
+  
+  # Bind the data frame to the overall results
+  all_results <- rbind(all_results, col_results)
+}
+
+# Export results
+write.csv(rf_results,
+          file = "Output/Data/Sites/csv/PassaicRiver/PassaicRiverRFPerformancePCB.csv",
+          row.names = FALSE)
+
+# Export combined results
+write.csv(all_results,
+          file = "Output/Data/Sites/csv/PassaicRiver/PassaicRiverRFObsPredPCB.csv",
+          row.names = FALSE)
+
+# Plot
+plotRFPCBi <- ggplot(all_results, aes(x = 10^(Actual), y = 10^(Predicted))) +
   geom_point(shape = 21, size = 3, fill = "white") +
-  scale_y_log10(limits = c(10, 10^6),
+  scale_y_log10(limits = c(0.1, 10^5),
                 breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x))) +
-  scale_x_log10(limits = c(10, 10^6),
+  scale_x_log10(limits = c(0.1, 10^5),
                 breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x))) +
-  xlab(expression(bold("Observed concentration " *Sigma*"PCB (pg/L)"))) +
-  ylab(expression(bold("Predicted lme concentration " *Sigma*"PCB (pg/L)"))) +
+  xlab(expression(bold("Observed concentration PCBi (pg/L)"))) +
+  ylab(expression(bold("Predicted lme concentration PCBi (pg/L)"))) +
   geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
-  geom_abline(intercept = 0.30103, slope = 1, col = "blue",
-              linewidth = 0.7) + # 1:2 line (factor of 2)
-  geom_abline(intercept = -0.30103, slope = 1, col = "blue",
-              linewidth = 0.7) + # 2:1 line (factor of 2)
+  geom_abline(intercept = 0.30103, slope = 1, col = "blue", linewidth = 0.7) + # 1:2 line (factor of 2)
+  geom_abline(intercept = -0.30103, slope = 1, col = "blue", linewidth = 0.7) + # 2:1 line (factor of 2)
   theme_bw() +
   theme(aspect.ratio = 15/15) +
   annotation_logticks(sides = "bl")
 
-# Estimate a factor of 2 between observations and predictions
-# Create a data frame with observed and predicted values
-compare_df.3 <- data.frame(observed = test_data$tPCB,
-                           predicted = 10^predictions.3)
+# Print the plot
+print(plotRFPCBi)
 
-# Estimate a factor of 2 between observations and predictions
-compare_df.3$factor2 <- compare_df.3$observed/compare_df.3$predicted
+# Save plot in folder
+ggsave("Output/Plots/Sites/ObsPred/PassaicRiver/PassaicRiverRFPCBV01.png",
+       plot = plotRFPCBi, width = 6, height = 5, dpi = 500)
 
-# Calculate the percentage of observations within the factor of 2
-factor2_percentage.3 <- nrow(compare_df.3[compare_df.3$factor2 > 0.5 & compare_df.3$factor2 < 2, ])/nrow(compare_df.3)*100
+
