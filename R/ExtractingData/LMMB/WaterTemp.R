d48806b6caa910a7bf9d75c30857ce21751f6817
martie <andres-martinez@uiowa.edu>
2023-12-12 19:17:45 -0600

    Improve code.

--- R/ExtractingData/LMMB/WaterTemp.R
+++ R/ExtractingData/LMMB/WaterTemp.R
@@ -3,47 +3,42 @@
 
 # Read the specific columns (YY, MM, DD, WTMP)
 # Temperature in C
-WT1993 <- read.table('Data/LMMB/OpenLake/WaterTemp/45002h1993.txt',
-                     header = TRUE,
-                     colClasses = c("numeric", "numeric",
-                                    "numeric",
-                                    "numeric"))[, c("YY", "MM", "DD", "WTMP")]
+{
+  WT1993 <- read.table('Data/LMMB/OpenLake/WaterTemp/45002h1993.txt',
+                       header = TRUE,
+                       colClasses = c("numeric", "numeric",
+                                      "numeric",
+                                      "numeric"))[, c("YY", "MM", "DD", "WTMP")]
+  
+  WT1994 <- read.table('Data/LMMB/OpenLake/WaterTemp/45002h1994.txt',
+                       header = TRUE,
+                       colClasses = c("numeric", "numeric",
+                                      "numeric",
+                                      "numeric"))[, c("YY", "MM", "DD", "WTMP")]
+  
+  WT1995 <- read.table('Data/LMMB/OpenLake/WaterTemp/45002h1995.txt',
+                       header = TRUE,
+                       colClasses = c("numeric", "numeric",
+                                      "numeric",
+                                      "numeric"))[, c("YY", "MM", "DD", "WTMP")]
+}
 
-WT1994 <- read.table('Data/LMMB/OpenLake/WaterTemp/45002h1994.txt',
-                     header = TRUE,
-                     colClasses = c("numeric", "numeric",
-                                    "numeric",
-                                    "numeric"))[, c("YY", "MM", "DD", "WTMP")]
-
-WT1995 <- read.table('Data/LMMB/OpenLake/WaterTemp/45002h1995.txt',
-                     header = TRUE,
-                     colClasses = c("numeric", "numeric",
-                                    "numeric",
-                                    "numeric"))[, c("YY", "MM", "DD", "WTMP")]
+# Combine all the datasets
+LakeMichiganWT <- rbind(WT1993, WT1994, WT1995)
 
 # Daily average, not including 999s
-WT1993 <- aggregate(WTMP ~ YY + MM + DD, data = WT1993, mean, subset = WTMP != 999)
-WT1994 <- aggregate(WTMP ~ YY + MM + DD, data = WT1994, mean, subset = WTMP != 999)
-WT1995 <- aggregate(WTMP ~ YY + MM + DD, data = WT1995, mean, subset = WTMP != 999)
+LakeMichiganWT <- aggregate(WTMP ~ YY + MM + DD, data = LakeMichiganWT,
+                            mean, subset = WTMP != 999)
 
 # Transforming temperature from C to K
-WT1993$WTMP_K <- WT1993$WTMP + 273.15
-WT1994$WTMP_K <- WT1994$WTMP + 273.15
-WT1995$WTMP_K <- WT1995$WTMP + 273.15
+LakeMichiganWT$WTMP_K <- LakeMichiganWT$WTMP + 273.15
 
 # Create a new column with date
-WT1993$Date <- as.Date(paste(WT1993$MM, WT1993$DD, WT1993$YY,
-                             sep = "/"), format = "%m/%d/%y")
-WT1994$Date <- as.Date(paste(WT1994$MM, WT1994$DD, WT1994$YY,
-                             sep = "/"), format = "%m/%d/%y")
-WT1995$Date <- as.Date(paste(WT1995$MM, WT1995$DD, WT1995$YY,
-                             sep = "/"), format = "%m/%d/%y")
+LakeMichiganWT$Date <- as.Date(paste(LakeMichiganWT$MM, LakeMichiganWT$DD,
+                                     LakeMichiganWT$YY, sep = "/"),
+                               format = "%m/%d/%y")
 
 # Export results
-write.csv(WT1993,
-          file = "Output/Data/Sites/csv/GreatLakes/WT1993.csv")
-write.csv(WT1994,
-          file = "Output/Data/Sites/csv/GreatLakes/WT1994.csv")
-write.csv(WT1995,
-          file = "Output/Data/Sites/csv/GreatLakes/WT1995.csv")
+write.csv(LakeMichiganWT,
+          file = "Output/Data/Sites/csv/GreatLakes/LakeMichiganWT.csv")
 
