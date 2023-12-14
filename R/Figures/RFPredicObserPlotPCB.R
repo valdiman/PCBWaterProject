cd5c3f8e0371abbc09860a73bc0a8ae79395df6b
martie <andres-martinez@uiowa.edu>
2023-12-14 16:20:01 -0600

    Improved version.

--- R/Figures/RFPredicObserPlotPCB.R
+++ R/Figures/RFPredicObserPlotPCB.R
@@ -13,47 +13,35 @@ install.packages("RColorBrewer")
 
 # Read generated data
 {
-  # Anacostia River
+  # 21 Mich
+  mic <- read.csv("Output/Data/Sites/csv/21Mich/21MichRFObsPredPCBV02.csv")
   # Bannister Federal Complex
-  #bfc <- read.csv("Output/Data/Sites/csv/BannisterFedComplex/BannisterFedComplexObsPredtPCB.csv")
-  #bfc <- bfc[, -1]
+  bfc <- read.csv("Output/Data/Sites/csv/BannisterFedComplex/BannisterFedComplexRFObsPredPCB.csv")
   # Chesapeake Bay data
-  #che <- read.csv("Output/Data/Sites/csv/ChesapeakeBay/ChesapeakeObsPredtPCB.csv")
-  #che <- che[, -1]
+  che <- read.csv("Output/Data/Sites/csv/ChesapeakeBay/ChesapeakeBayRFObsPredPCBV01.csv")
   # Fox River data
   fox <- read.csv("Output/Data/Sites/csv/FoxRiver/FoxRiverRFObsPredPCB.csv")
-  fox <- fox[, -2]
   # Great Lakes
-  grl <- read.csv("Output/Data/Sites/csv/GreatLakes/GreatLakesRFObsPredPCB.csv")
-  grl <- grl[, -2]
+  grl <- read.csv("Output/Data/Sites/csv/GreatLakes/GreatLakesRFObsPredPCBV02.csv")
   # Hudson River
   hud <- read.csv("Output/Data/Sites/csv/HudsonRiver/HudsonRiverRFObsPredPCB.csv")
-  hud <- hud[, -2]
   # Lake Washington
-  #lwa <- read.csv("Output/Data/Sites/csv/LakeWashington/LakeWashingtonObsPredtPCB.csv")
-  #lwa <- lwa[, -1]
+  lwa <- read.csv("Output/Data/Sites/csv/LakeWashington/LakeWashingtonRFObsPredPCB.csv")
   # New Bedford Harbor data
   nbh <- read.csv("Output/Data/Sites/csv/NewBedfordHarbor/NBHRFObsPredPCB.csv")
-  nbh <- nbh[, -2]
   # Passaic River
   pas <- read.csv("Output/Data/Sites/csv/PassaicRiver/PassaicRiverRFObsPredPCB.csv")
-  pas <- pas[, -2]
   # Portland Harbord data
-  por <- read.csv("Output/Data/Sites/csv/PortlandHarbor/PortlandHarborRFObsPredPCB.csv")
-  por <- por[, -2]
-  # Richardson Hill Road Landfill
-  #rhr <- read.csv("Output/Data/Sites/csv/Richardson/RichardsonObsPredtPCB.csv")
-  #rhr <- rhr[, -1]
+  por <- read.csv("Output/Data/Sites/csv/PortlandHarbor/PortlandHarborRFObsPredPCBV02.csv")
   # Spokane River data
-  spo <- read.csv("Output/Data/Sites/csv/SpokaneRiver/SpokaneRiverRFObsPredPCB.csv")
-  spo <- spo[, -2]
+  spo <- read.csv("Output/Data/Sites/csv/SpokaneRiver/SpokaneRiverRFObsPredPCBV02.csv")
   # Combine the data frames
-  combined_data <- rbind(fox, grl, hud, nbh, pas, por, spo)
+  combined_data <- rbind(mic, bfc, che, fox, grl, hud, lwa, nbh, pas, por, spo)
 }
 
-# Create a custom color palette with 12 different colors
-n_colors <- 7
-custom_palette <- colorRampPalette(brewer.pal(7, "Set1"))(n_colors)
+# Create a custom color palette with 9 different colors
+n_colors <- 11
+custom_palette <- colorRampPalette(brewer.pal(9, "Set1"))(n_colors)
 
 # Add a new column with the number of rows for each Location
 combined_data$Location <- paste0(combined_data$Location,
@@ -72,7 +60,7 @@ CombinePredObsPlot <- ggplot(combined_data,
                 labels = trans_format("log10", math_format(10^.x))) +
   scale_fill_manual(values = custom_palette) +
   xlab(expression(bold("Observed concentration PCBi (pg/L)"))) +
-  ylab(expression(bold("Predicted LME concentration PCBi (pg/L)"))) +
+  ylab(expression(bold("Predicted concentration PCBi (pg/L)"))) +
   geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
   geom_abline(intercept = log10(2), slope = 1, col = "blue", linewidth = 0.7) +
   geom_abline(intercept = log10(0.5), slope = 1, col = "blue", linewidth = 0.7) +
@@ -82,7 +70,7 @@ CombinePredObsPlot <- ggplot(combined_data,
         axis.title = element_text(size = 20),
         legend.title = element_text(size = 18),
         legend.text = element_text(size = 18),
-        legend.position = c(1.35, 0.75),  # Adjust the position (values are between 0 and 1)
+        legend.position = c(1.28, 0.78),  # Adjust the position (values are between 0 and 1)
         legend.background = element_rect(fill = "transparent")) +  # Make the legend background transparent
   annotation_logticks(sides = "bl")
 
@@ -90,6 +78,6 @@ CombinePredObsPlot <- ggplot(combined_data,
 print(CombinePredObsPlot)
 
 # Save plot
-ggsave("Output/Figures/Sites/CombineRFObsPredPCBi.png",
+ggsave("Output/Figures/Sites/CombineRFObsPredPCBiV02.png",
        plot = CombinePredObsPlot, width = 18, height = 8, dpi = 500)
 
