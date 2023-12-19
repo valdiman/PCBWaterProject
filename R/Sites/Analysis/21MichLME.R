5a7941a4c4f463505386ee007dad2617755180db
martie <andres-martinez@uiowa.edu>
2023-12-19 13:28:49 -0600

    New version, just lme analysis.

---
+++ R/Sites/Analysis/21MichLME.R
@@ -0,0 +1,419 @@
+## LME analysis
+## Data from DEQ Michigan
+
+# Install packages
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("robustbase")
+install.packages("dplyr")
+install.packages("tibble")
+install.packages("Matrix")
+install.packages("lme4")
+install.packages("MuMIn")
+install.packages("lmerTest")
+install.packages("zoo")
+install.packages("dataRetrieval")
+install.packages("reshape")
+install.packages("tidyr")
+install.packages('patchwork')
+install.packages("scales")
+install.packages("sf")
+install.packages("sfheaders")
+
+# Load libraries
+{
+  library(ggplot2)
+  library(scales) # function trans_breaks
+  library(stringr) # str_detect
+  library(robustbase) # function colMedians
+  library(dplyr) # performs %>%
+  library(tibble) # adds a column
+  library(lme4) # performs lme
+  library(MuMIn) # gets Rs from lme
+  library(lmerTest) # gets the p-value from lme
+  library(zoo) # yields seasons
+  library(dataRetrieval) # read data from USGS
+  library(reshape)
+  library(tidyr) # function gather
+  library(patchwork) # combine plots
+  library(sf) # Create file to be used in Google Earth
+  library(sfheaders) # Create file to be used in Google Earth
+}
+
+# Read data ---------------------------------------------------------------
+# Data in pg/L
+wdc <- read.csv("Data/WaterDataCongenerAroclor09072023.csv")
+
+# Select 21 Michigan data ---------------------------------------------------
+mic <- wdc[str_detect(wdc$LocationName, '21Mich'),]
+
+# Data preparation --------------------------------------------------------
+{
+  # Change date format
+  mic$SampleDate <- as.Date(mic$SampleDate, format = "%m/%d/%y")
+  # Calculate sampling time
+  time.day <- data.frame(as.Date(mic$SampleDate) - min(as.Date(mic$SampleDate)))
+  # Create individual code for each site sampled
+  site.numb <- mic$SiteID %>% as.factor() %>% as.numeric
+  # Include season
+  yq.s <- as.yearqtr(as.yearmon(mic$SampleDate, "%m/%d/%Y") + 1/12)
+  season.s <- factor(format(yq.s, "%q"), levels = 1:4,
+                     labels = c("0", "S-1", "S-2", "S-3")) # winter, spring, summer, fall
+  # Create data frame
+  mic.tpcb <- cbind(factor(mic$SiteID), mic$SampleDate,
+                    mic$Latitude, mic$Longitude, as.matrix(mic$tPCB),
+                    data.frame(time.day), site.numb, season.s)
+  # Add column names
+  colnames(mic.tpcb) <- c("SiteID", "date", "Latitude", "Longitude",
+                          "tPCB", "time", "site.code", "season")
+}
+
+# Get coordinates per site to plot in Google Earth
+location <- mic.tpcb[c('SiteID', 'Latitude', 'Longitude', 'tPCB')]
+# Average tPCB per site
+location <- aggregate(tPCB ~ SiteID + Latitude + Longitude,
+                      data = location, mean)
+# Create an sf data frame
+sf_location <- st_as_sf(location, coords = c("Longitude", "Latitude"))
+# Set the CRS to WGS 84 (EPSG:4326)
+sf_location <- st_set_crs(sf_location, 4326)
+# Define the full file path for the KML file
+kmlFilePath <- "Output/Data/Sites/GoogleEarth/21MichLocations.kml"
+# Write the KML file to the specified directory
+st_write(sf_location, kmlFilePath, driver = "kml", append = FALSE)
+
+# tPCB Regressions --------------------------------------------------------
+# Perform Linear Mixed-Effects Model (lme)
+# Get variables
+tpcb <- mic.tpcb$tPCB
+time <- mic.tpcb$time
+site <- mic.tpcb$site.code
+season <- mic.tpcb$season
+# tPCB vs. time + season + site
+lme.mic.tpcb <- lmer(log10(tpcb) ~ 1 + time + season + (1|site),
+                     REML = FALSE,
+                     control = lmerControl(check.nobs.vs.nlev = "ignore",
+                                           check.nobs.vs.rankZ = "ignore",
+                                           check.nobs.vs.nRE="ignore"))
+
+# See results
+summary(lme.mic.tpcb)
+# Look at residuals
+{
+  res.mic.tpcb <- resid(lme.mic.tpcb) # get list of residuals
+  # Create Q-Q plot for residuals
+  # Create pdf file
+  pdf("Output/Plots/Sites/Q-Q/21MitchQ-QtPCB.pdf")
+  qqnorm(res.mic.tpcb,
+         main = expression(paste("Normal Q-Q Plot (log"[10]* Sigma,
+                                 "PCB)")))
+  # Add a straight diagonal line to the plot
+  qqline(res.mic.tpcb)
+  dev.off()
+}
+# Shapiro test
+shapiro.test(resid(lme.mic.tpcb))
+# lme doesn't work here. p-value = 5e-13
+
+# Individual PCB Analysis -------------------------------------------------
+# Prepare data.frame
+{
+  # Remove metadata
+  mic.pcb <- subset(mic, select = -c(SampleID:AroclorCongener))
+  # Remove Aroclor data
+  mic.pcb <- subset(mic.pcb, select = -c(A1016:tPCB))
+  # Log10 individual PCBs 
+  mic.pcb <- log10(mic.pcb)
+  # Replace -inf to NA
+  mic.pcb <- do.call(data.frame,
+                     lapply(mic.pcb,
+                            function(x) replace(x, is.infinite(x), NA)))
+  # Remove individual PCB that have 30% or less NA values
+  mic.pcb.1 <- mic.pcb[,
+                       -which(colSums(is.na(mic.pcb))/nrow(mic.pcb) > 0.7)]
+  # Add site ID
+  SiteID <- factor(mic$SiteID)
+  # Change date format
+  SampleDate <- as.Date(mic$SampleDate, format = "%m/%d/%y")
+  # Calculate sampling time
+  time.day <- data.frame(as.Date(SampleDate) - min(as.Date(SampleDate)))
+  # Change name time.day to time
+  colnames(time.day) <- "time"
+  # Create individual code for each site sampled
+  site.numb <- mic$SiteID %>% as.factor() %>% as.numeric
+  # Include season
+  yq.s <- as.yearqtr(as.yearmon(mic$SampleDate, "%m/%d/%Y") + 1/12)
+  season.s <- factor(format(yq.s, "%q"), levels = 1:4,
+                     labels = c("0", "S-1", "S-2", "S-3")) # winter, spring, summer, fall
+  # Add date and time to fox.pcb.1
+  mic.pcb.1 <- cbind(mic.pcb.1, SiteID, SampleDate, data.frame(time.day),
+                     site.numb, season.s)
+  # Remove metadata
+  mic.pcb.2 <- subset(mic.pcb.1, select = -c(SiteID:season.s))
+}
+
+# LME for individual PCBs -------------------------------------------------
+# Get covariates
+time <- mic.pcb.1$time
+season <- mic.pcb.1$season
+site <- mic.pcb.1$site.numb
+
+# Create matrix to store results
+lme.pcb <- matrix(nrow = length(mic.pcb.2[1,]), ncol = 19)
+
+# Perform LME
+for (i in 1:length(mic.pcb.2[1,])) {
+  fit <- lmer(mic.pcb.2[, i] ~ 1 + time + season + (1|site),
+              REML = FALSE,
+              control = lmerControl(check.nobs.vs.nlev = "ignore",
+                                    check.nobs.vs.rankZ = "ignore",
+                                    check.nobs.vs.nRE="ignore"))
+  lme.pcb[i, 1] <- fixef(fit)[1] # intercept
+  lme.pcb[i, 2] <- summary(fit)$coef[1, "Std. Error"] # intercept error
+  lme.pcb[i, 3] <- summary(fit)$coef[1, "Pr(>|t|)"] # intercept p-value
+  lme.pcb[i, 4] <- fixef(fit)[2] # time
+  lme.pcb[i, 5] <- summary(fit)$coef[2, "Std. Error"] # time error
+  lme.pcb[i, 6] <- summary(fit)$coef[2, "Pr(>|t|)"] # time p-value
+  lme.pcb[i, 7] <- fixef(fit)[3] # season 2
+  lme.pcb[i, 8] <- summary(fit)$coef[3, "Std. Error"] # season 2 error
+  lme.pcb[i, 9] <- summary(fit)$coef[3, "Pr(>|t|)"] # # season 2 p-value
+  lme.pcb[i, 10] <- fixef(fit)[4] # season 3
+  lme.pcb[i, 11] <- summary(fit)$coef[4, "Std. Error"] # season 3 error
+  lme.pcb[i, 12] <- summary(fit)$coef[4, "Pr(>|t|)"] # season 3 p-value
+  lme.pcb[i, 13] <- -log(2)/lme.pcb[i, 4]/365 # t0.5
+  lme.pcb[i, 14] <- abs(-log(2)/lme.pcb[i, 4]/365)*lme.pcb[i,5]/abs(lme.pcb[i,4]) # t0.5 error
+  lme.pcb[i, 15] <- as.data.frame(VarCorr(fit))[1,'sdcor']
+  lme.pcb[i, 16] <- as.data.frame(r.squaredGLMM(fit))[1, 'R2m']
+  lme.pcb[i, 17] <- as.data.frame(r.squaredGLMM(fit))[1, 'R2c']
+  lme.pcb[i, 18] <- shapiro.test(resid(fit))$p.value
+  # Calculate RMSE
+  # Predictions
+  predictions <- predict(fit)
+  # Calculate residuals and RMSE
+  residuals <- mic.pcb.2[, i] - predictions
+  non_na_indices <- !is.na(residuals)
+  lme.pcb[i, 19] <- sqrt(mean(residuals[non_na_indices]^2))
+}
+
+# Change number format to 3 significant figures
+lme.pcb <- formatC(signif(lme.pcb, digits = 3))
+
+# Transform result to data.frame so factor 2 can be included
+lme.pcb <- as.data.frame(lme.pcb)
+
+# Add factor of 2
+# Create matrix to store results
+lme.fit.pcb <- matrix(nrow = length(mic.pcb.2[,1]),
+                      ncol = length(mic.pcb.2[1,]))
+
+# Create a vector to store factor 2 for each congener
+factor2_vector <- numeric(length = length(mic.pcb.2[1,]))
+
+for (i in 1:length(mic.pcb.2[1,])) {
+  fit <- lmer(mic.pcb.2[,i] ~ 1 + time + season + (1|site),
+              REML = FALSE,
+              control = lmerControl(check.nobs.vs.nlev = "ignore",
+                                    check.nobs.vs.rankZ = "ignore",
+                                    check.nobs.vs.nRE="ignore"),
+              na.action = na.exclude)
+  
+  lme.fit.pcb[,i] <- fitted(fit)
+  
+  # Calculate factor2 for each congener
+  factor2 <- 10^(lme.fit.pcb[, i])/10^(mic.pcb.2[, i])
+  factor2_vector[i] <- sum(factor2 > 0.5 & factor2 < 2,
+                           na.rm = TRUE) / (sum(!is.na(factor2))) * 100
+}
+
+# Add factor 2 to lme.pcb data.frame
+lme.pcb$factor2 <- factor2_vector
+
+# Change number format of factor 2 to 3 significant figures
+lme.pcb$factor2 <- formatC(signif(lme.pcb$factor2, digits = 3))
+
+# Add congener names
+congeners <- colnames(mic.pcb.2)
+lme.pcb <- as.data.frame(cbind(congeners, lme.pcb))
+
+# Add column names
+colnames(lme.pcb) <- c("Congeners", "Intercept", "Intercept.error",
+                       "Intercept.pv", "time", "time.error", "time.pv",
+                       "season2", "season2.error", "season2.pv", "season3",
+                       "season3.error", "season3.pv", "t05", "t05.error",
+                       "RandonEffectSiteStdDev", "R2nR", "R2R", "Normality",
+                       "RMSE", "Factor2")
+
+# Remove congeners with no normal distribution
+# Shapiro test p-value < 0.05
+lme.pcb$Normality <- as.numeric(lme.pcb$Normality)
+# Get the congeners that are not showing normality
+lme.pcb.out <- lme.pcb[lme.pcb$Normality < 0.05, ]
+lme.pcb <- lme.pcb[lme.pcb$Normality > 0.05, ]
+
+# Export results
+write.csv(lme.pcb, file = "Output/Data/Sites/csv/21Mich/21MichLmePCB.csv")
+
+# Individual PCB congener plots -------------------------------------------
+# Generate predictions
+# Select congeners that are not showing normality to be remove from mic.pcb.2
+df <- data.frame(names_to_remove = lme.pcb.out$Congeners)
+# Get column indices to remove
+cols_to_remove <- which(names(mic.pcb.2) %in% df$names_to_remove)
+# Remove columns from mic.pcb.2 with congeners that don't show normality
+mic.pcb.3 <- mic.pcb.2[, -cols_to_remove]
+
+# Create matrix to store results
+lme.fit.pcb <- matrix(nrow = length(mic.pcb.3[,1]),
+                      ncol = length(mic.pcb.3[1,]))
+
+for (i in 1:length(mic.pcb.3[1,])) {
+  fit <- lmer(mic.pcb.3[,i] ~ 1 + time + season + (1|site),
+              REML = FALSE,
+              control = lmerControl(check.nobs.vs.nlev = "ignore",
+                                    check.nobs.vs.rankZ = "ignore",
+                                    check.nobs.vs.nRE="ignore"),
+              na.action = na.exclude)
+  lme.fit.pcb[,i] <- fitted(fit)
+}
+
+# Estimate the overall factor of 2 between observations and predictions
+factor2 <- 10^(mic.pcb.3)/10^(lme.fit.pcb)
+factor2.pcb <- sum(factor2 > 0.5 & factor2 < 2,
+                   na.rm = TRUE)/(sum(!is.na(factor2)))*100
+
+# (1)
+# Plot 1:1 for all congeners
+# Transform lme.fit.pcb to data.frame
+lme.fit.pcb <- as.data.frame(lme.fit.pcb)
+# Add congener names to lme.fit.pcb columns
+colnames(lme.fit.pcb) <- colnames(mic.pcb.3)
+# Add code number to first column
+df1 <- cbind(code = row.names(mic.pcb.3), mic.pcb.3)
+df2 <- cbind(code = row.names(lme.fit.pcb), lme.fit.pcb)
+
+for (i in 2:length(df1)) {
+  col_name <- if (i == 1) {
+    ""  # leave the name empty for the first plot
+  } else {
+    names(df1)[i] # use the column name for other plots
+  }
+  
+  # create plot for each pair of columns
+  p <- ggplot(data = data.frame(x = df1$code, y1 = 10^(df1[, i]), y2 = 10^(df2[, i])),
+              aes(x = y1, y = y2)) +
+    geom_point(shape = 21, size = 3, fill = "white") +
+    scale_y_log10(limits = c(0.01, 10^4), breaks = trans_breaks("log10", function(x) 10^x),
+                  labels = trans_format("log10", math_format(10^.x))) +
+    scale_x_log10(limits = c(0.01, 10^4), breaks = trans_breaks("log10", function(x) 10^x),
+                  labels = trans_format("log10", math_format(10^.x))) +
+    xlab(expression(bold("Observed concentration PCBi (pg/L)"))) +
+    ylab(expression(bold("Predicted lme concentration PCBi (pg/L)"))) +
+    theme_bw() +
+    theme(aspect.ratio = 15/15) +
+    annotation_logticks(sides = "bl") +
+    geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
+    geom_abline(intercept = log10(2), slope = 1, col = "blue", linewidth = 0.7) + # 1:2 line (factor of 2)
+    geom_abline(intercept = log10(0.5), slope = 1, col = "blue", linewidth = 0.7) +
+    annotate('text', x = 0.5, y = 10^4, label = gsub("\\.", "+", names(df1)[i]),
+             size = 3, fontface = 2)
+  # save plot
+  ggsave(paste0("Output/Plots/Sites/ObsPred/21Mich/", col_name, ".png"), plot = p,
+         width = 6, height = 6, dpi = 500)
+}
+
+# (2)
+# All plots in one page
+# Create a list to store all the plots
+plot_list <- list()
+
+# Loop over the columns of df1 and df2
+for (i in 2:length(df1)) {
+  col_name <- paste(names(df1)[i], sep = "")  # use the column name for plot title
+  # Create plot for each pair of columns and add to plot_list
+  p <- ggplot(data = data.frame(x = df1$code, y1 = 10^(df1[, i]), y2 = 10^(df2[, i])),
+              aes(x = y1, y = y2)) +
+    geom_point(shape = 21, size = 3, fill = "white") +
+    scale_y_log10(limits = c(0.01, 10^4), breaks = trans_breaks("log10", function(x) 10^x),
+                  labels = trans_format("log10", math_format(10^.x))) +
+    scale_x_log10(limits = c(0.01, 10^4), breaks = trans_breaks("log10", function(x) 10^x),
+                  labels = trans_format("log10", math_format(10^.x))) +
+    xlab(expression(bold("Observed concentration PCBi (pg/L)"))) +
+    ylab(expression(bold("Predicted lme concentration PCBi (pg/L)"))) +
+    theme_bw() +
+    theme(aspect.ratio = 15/15, 
+          axis.title = element_text(size = 8)) +
+    annotation_logticks(sides = "bl") +
+    annotate('text', x = 0.5, y = 10^4, label = gsub("\\.", "+", col_name),
+             size = 2.5, fontface = 2) +
+    geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
+    geom_abline(intercept = log10(2), slope = 1, col = "blue", linewidth = 0.7) + # 1:2 line (factor of 2)
+    geom_abline(intercept = log10(0.5), slope = 1, col = "blue", linewidth = 0.7)
+  
+  plot_list[[i-1]] <- p  # add plot to list
+}
+# Combine all the plots using patchwork
+combined_plot <- wrap_plots(plotlist = plot_list, ncol = 4)
+# Save the combined plot
+ggsave("Output/Plots/Sites/ObsPred/21Mich/combined_plot.png", plot = combined_plot,
+       width = 15, height = 15, dpi = 500)
+
+# (3)
+# Create a list to store all the cleaned data frames
+cleaned_df_list <- list()
+# Loop over the columns of df1 and df2
+for (i in 2:length(df1)) {
+  # Create a new data frame by binding the columns of df1 and df2 for each pair of columns
+  df_pair <- cbind(df1[,1], df1[,i], df2[,i])
+  colnames(df_pair) <- c("code", "observed", "predicted")
+  # Remove the rows with missing values
+  cleaned_df_pair <- na.omit(df_pair)
+  # Add the cleaned data frame to the list
+  cleaned_df_list[[i-1]] <- cleaned_df_pair
+}
+
+{
+  # Modify data to be plotted
+  # Combine all the cleaned data frames using rbind
+  combined_cleaned_df <- do.call(rbind, cleaned_df_list)
+  # Convert the matrix to a data frame
+  combined_cleaned_df <- as.data.frame(combined_cleaned_df)
+  # Convert the code column to a factor
+  combined_cleaned_df$code <- as.factor(combined_cleaned_df$code)
+  # Convert the observed and predicted columns to numeric
+  combined_cleaned_df[,2:3] <- apply(combined_cleaned_df[,2:3], 2, as.numeric)
+}
+
+# Export results for plotting
+# Add column LocationName
+combined_cleaned_df$LocationName <- "21 Mich"
+write.csv(combined_cleaned_df,
+          file = "Output/Data/Sites/csv/21Mich/21MichObsPredPCB.csv")
+
+# Plot all the pairs together
+p <- ggplot(combined_cleaned_df, aes(x = 10^(observed), y = 10^(predicted))) +
+  geom_point(shape = 21, size = 3, fill = "white") +
+  scale_y_log10(limits = c(0.01, 10^4), 
+                breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  scale_x_log10(limits = c(0.01, 10^4), 
+                breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  xlab(expression(bold("Observed concentration PCBi (pg/L)"))) +
+  ylab(expression(bold("Predicted lme concentration PCBi (pg/L)"))) +
+  theme_bw() +
+  theme(aspect.ratio = 15/15, 
+        axis.title = element_text(size = 10)) +
+  annotation_logticks(sides = "bl") +
+  geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
+  geom_abline(intercept = log10(2), slope = 1, col = "blue", linewidth = 0.7) + # 1:2 line (factor of 2)
+  geom_abline(intercept = log10(0.5), slope = 1, col = "blue", linewidth = 0.7) +
+  annotate("text", x = 1, y = 10^3.7,
+           label = expression(atop("21 Mich",
+                                   paste("7 PCB congeners (n = 214 pairs)"))),
+           size = 4, fontface = 2)
+# See plot
+print(p)
+# Save plot
+ggsave("Output/Plots/Sites/ObsPred/21Mich/21MichObsPredPCBV02.png",
+       plot = p, width = 8, height = 8, dpi = 500)
+
