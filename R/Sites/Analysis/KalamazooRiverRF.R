250643564c89f0a0cb659fe0333043f3b836ae3c
martie <andres-martinez@uiowa.edu>
2024-01-03 16:26:43 -0600

    new version.

---
+++ R/Sites/Analysis/KalamazooRiverRF.R
@@ -0,0 +1,208 @@
+## Water PCB concentrations data analysis per site
+## Kalamazoo River
+## Source: Allied Paper, Inc. Recycling of carbonless copy paper
+## Aroclors 1242, 1254 and 1260, no congener analysis
+## Random forest model
+
+# Install packages
+install.packages("randomForest")
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("robustbase")
+install.packages("dplyr")
+install.packages("tibble")
+install.packages("Matrix")
+install.packages("zoo")
+install.packages("dataRetrieval")
+install.packages("reshape")
+install.packages("tidyr")
+install.packages('patchwork')
+install.packages("scales")
+install.packages("sf")
+install.packages("units")
+install.packages("sfheaders")
+
+# Load libraries
+{
+  library(ggplot2)
+  library(scales) # function trans_breaks
+  library(stringr) # str_detect
+  library(robustbase) # function colMedians
+  library(dplyr) # performs %>%
+  library(tibble) # adds a column
+  library(zoo) # yields seasons
+  library(dataRetrieval) # read data from USGS
+  library(reshape)
+  library(tidyr) # function gather
+  library(patchwork) # combine plots
+  library(sf) # Create file to be used in Google Earth
+  library(units)
+  library(randomForest)
+}
+
+# Read data ---------------------------------------------------------------
+# Data in pg/L
+wdc <- read.csv("Data/WaterDataCongenerAroclor09072023.csv")
+
+# Select Kalamazoo data ---------------------------------------------------
+kal <- wdc[str_detect(wdc$LocationName, 'Kalamazoo River'),]
+# Superfund site from Morrow Dam (Kalamazoo River) to Lake Michigan
+# and 30 miles of Portage Creek (south), Cork St and Portage Creek Cork St sites
+# Dredging occurred at Plainwell Dam site.
+
+# Add distance to source --------------------------------------------------
+{
+  # Define source coordinates
+  source1 <- c(Latitude =  42.270024, Longitude = -85.575727)  # Allied Paper, Inc.
+  # Create an sf point for the source
+  source1_sf <- st_sfc(st_point(c(source1["Longitude"], source1["Latitude"])))
+  # Set the CRS to EPSG:4326
+  st_crs(source1_sf) <- 4326
+  # Transform source1_sf to UTM Zone 16N (EPSG:32616)
+  source1_sf_utm <- st_transform(source1_sf, 32616)
+  # Convert the data frame to an sf object
+  sf_kal <- st_as_sf(kal, coords = c("Longitude", "Latitude"))
+  # Set the CRS to WGS 84 (EPSG:4326)
+  sf_kal <- st_set_crs(sf_kal, 4326)
+  # Transform to UTM Zone 16N
+  sf_kal_utm <- st_transform(sf_kal, 32616)
+  # Calculate distances in meters from each location to the source
+  distances_meters <- st_distance(sf_kal_utm, source1_sf_utm)
+  # Convert distances to kilometers
+  distances_km <- units::set_units(distances_meters, "km")
+  # Extract numeric values and assign to the DistanceToSource column
+  kal$DistanceToSource <- as.numeric(distances_km[, 1])
+}
+
+# Data preparation --------------------------------------------------------
+{
+  # Change date format
+  kal$SampleDate <- as.Date(kal$SampleDate, format = "%m/%d/%y")
+  # Calculate sampling time
+  time.day <- data.frame(as.Date(kal$SampleDate) - min(as.Date(kal$SampleDate)))
+  # Create individual code for each site sampled
+  site.numb <- kal$SiteID %>% as.factor() %>% as.numeric
+  # Include season
+  yq.s <- as.yearqtr(as.yearmon(kal$SampleDate, "%m/%d/%Y") + 1/12)
+  season.s <- factor(format(yq.s, "%q"), levels = 1:4,
+                     labels = c("0", "S-1", "S-2", "S-3")) # winter, spring, summer, fall
+  # Create data frame
+  kal.tpcb <- cbind(factor(kal$SiteID), kal$SampleDate, as.matrix(kal$tPCB),
+                    data.frame(time.day), site.numb, season.s, kal$DistanceToSource)
+  # Add column names
+  colnames(kal.tpcb) <- c("SiteID", "date", "tPCB", "time", "site.code",
+                          "season", "DistanceToSource")
+}
+
+# Include USGS flow data --------------------------------------------------
+# Include flow data from USGS station Kalamazoo River, no temperature available
+{
+  siteKalN1 <- "04108660" # KALAMAZOO RIVER AT NEW RICHMOND, MI
+  siteKalN2 <- "04106000" # KALAMAZOO RIVER AT COMSTOCK, MI
+  # Codes to retrieve data
+  paramflow <- "00060" # discharge
+  # Flow (ft3/s)
+  flow.1 <- readNWISdv(siteKalN1, paramflow,
+                       min(kal.tpcb$date), max(kal.tpcb$date))
+  flow.2 <- readNWISdv(siteKalN2, paramflow,
+                       min(kal.tpcb$date), max(kal.tpcb$date))
+  kal.tpcb$flow.1 <- 0.03*flow.1$X_00060_00003[match(kal.tpcb$date,
+                                                       flow.1$Date)]
+  kal.tpcb$flow.2 <- 0.03*flow.2$X_00060_00003[match(kal.tpcb$date,
+                                                       flow.2$Date)]
+  # Create flow, flow.3
+  kal.tpcb$flow.3 <- ifelse(is.na(kal.tpcb$flow.1) == TRUE,
+                              kal.tpcb$flow.2/0.46, kal.tpcb$flow.1)
+  # Remove samples with flow.1 = NA
+  kal.tpcb.1 <- na.omit(kal.tpcb)
+}
+
+# Random Forest Model -----------------------------------------------------
+# Train-Test Split
+set.seed(123)
+train_indices <- sample(1:nrow(kal.tpcb), 0.8 * nrow(kal.tpcb))
+train_data <- kal.tpcb[train_indices, ]
+test_data <- kal.tpcb[-train_indices, ]
+
+# Fit the Model
+rf_model.1 <- randomForest(log10(tPCB) ~ time + site.code + flow.3 +
+                             DistanceToSource, data = train_data)
+
+# Make Predictions
+predictions.1 <- predict(rf_model.1, newdata = test_data)
+
+# Evaluate Model Performance
+mse.1 <- mean((predictions.1 - log10(test_data$tPCB))^2)
+rmse.1 <- sqrt(mse.1)
+r_squared.1 <- 1 - (sum((log10(test_data$tPCB) - predictions.1)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
+
+# Estimate a factor of 2 between observations and predictions
+# Create a data frame with observed and predicted values
+compare_df.1 <- data.frame(observed = test_data$tPCB,
+                           predicted = 10^predictions.1)
+
+# Estimate a factor of 2 between observations and predictions
+compare_df.1$factor2 <- compare_df.1$observed/compare_df.1$predicted
+
+# Calculate the percentage of observations within the factor of 2
+factor2_percentage.1 <- nrow(compare_df.1[compare_df.1$factor2 > 0.5 & compare_df.1$factor2 < 2, ])/nrow(compare_df.1)*100
+
+# Create the data frame directly
+performance_df <- data.frame(Heading = c("RMSE", "R2", "Factor2"),
+                             Value = c(rmse.1, r_squared.1,
+                                       factor2_percentage.1))
+
+# Remove unnecessary columns
+performance_df <- performance_df[, !(names(performance_df) %in% c("V1", "V2", "V3"))]
+
+# Print the modified data frame
+print(performance_df)
+
+# Export results
+write.csv(performance_df,
+          file = "Output/Data/Sites/csv/KalamazooRiver/KalamazooRiverRFPerformancetPCB.csv")
+
+# Feature Importance
+importance.1 <- importance(rf_model.1)
+# Plot features
+barplot(importance.1[, 1], names.arg = rownames(importance.1),
+        main = "Feature Importance", las = 2, cex.names = 0.7)
+
+# Create a data frame for plotting and exporting
+plot_data.1 <- data.frame(
+  Location = rep("Kalamazoo River", nrow(test_data)),
+  Actual = log10(test_data$tPCB),
+  Predicted = predictions.1
+)
+
+# Export results
+write.csv(plot_data.1,
+          file = "Output/Data/Sites/csv/KalamazooRiver/KalamazooRiverRFObsPredtPCB.csv",
+          row.names = FALSE)
+
+# Create the scatter plot using ggplot2
+plotRF <- ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
+  geom_point(shape = 21, size = 3, fill = "white") +
+  scale_y_log10(limits = c(10, 10^7),
+                breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  scale_x_log10(limits = c(10, 10^7),
+                breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  xlab(expression(bold("Observed concentration " *Sigma*"PCB (pg/L)"))) +
+  ylab(expression(bold("Predicted lme concentration " *Sigma*"PCB (pg/L)"))) +
+  geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
+  geom_abline(intercept = 0.30103, slope = 1, col = "blue",
+              linewidth = 0.7) + # 1:2 line (factor of 2)
+  geom_abline(intercept = -0.30103, slope = 1, col = "blue",
+              linewidth = 0.7) + # 2:1 line (factor of 2)
+  theme_bw() +
+  theme(aspect.ratio = 15/15) +
+  annotation_logticks(sides = "bl")
+
+# Print the plot
+print(plotRF)
+
+# Save plot in folder
+ggsave("Output/Plots/Sites/ObsPred/KalamazooRiver/KalamazooRiverRFtPCBV01.png",
+       plot = plotRF, width = 6, height = 5, dpi = 500)
