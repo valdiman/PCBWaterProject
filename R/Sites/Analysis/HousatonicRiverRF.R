65c30695be8901fbb84c87909c1711d95402474e
martie <andres-martinez@uiowa.edu>
2024-02-16 15:10:10 -0600

    small changes

--- R/Sites/Analysis/HousatonicRiverRF.R
+++ R/Sites/Analysis/HousatonicRiverRF.R
@@ -197,5 +197,5 @@ plotRF <- ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
 print(plotRF)
 
 # Save plot in folder
-ggsave("Output/Plots/Sites/ObsPred/HousatonicRiver/HousatonicRiverRFV01.png",
+ggsave("Output/Plots/Sites/ObsPred/HousatonicRiver/HousatonicRiverRFtPCB.png",
        plot = plotRF, width = 6, height = 5, dpi = 500)
