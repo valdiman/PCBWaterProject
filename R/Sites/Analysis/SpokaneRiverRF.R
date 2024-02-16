4175063d6b5e099be595f10864558e5af8bbf7df
martie <andres-martinez@uiowa.edu>
2024-02-16 15:23:38 -0600

    small changes

--- R/Sites/Analysis/SpokaneRiverRF.R
+++ R/Sites/Analysis/SpokaneRiverRF.R
@@ -225,7 +225,7 @@ plotRF <- ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
 print(plotRF)
 
 # Save plot in folder
-ggsave("Output/Plots/Sites/ObsPred/SpokaneRiver/SpokaneRiverRFtPCBV02.png",
+ggsave("Output/Plots/Sites/ObsPred/SpokaneRiver/SpokaneRiverRFtPCB.png",
        plot = plotRF, width = 6, height = 5, dpi = 500)
 
 # Individual PCB Analysis -------------------------------------------------
@@ -403,6 +403,6 @@ plotRFPCBi <- ggplot(all_results, aes(x = 10^(Actual), y = 10^(Predicted))) +
 print(plotRFPCBi)
 
 # Save plot in folder
-ggsave("Output/Plots/Sites/ObsPred/SpokaneRiver/SpokaneRiverRFPCBV02.png",
+ggsave("Output/Plots/Sites/ObsPred/SpokaneRiver/SpokaneRiverRFPCB.png",
        plot = plotRFPCBi, width = 6, height = 5, dpi = 500)
 
