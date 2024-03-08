5deea1b688e228ed4bd4a08572a0837c040df5c2
martie <andres-martinez@uiowa.edu>
2024-03-08 12:09:16 -0600

    New version. Working!

--- R/Global/DataAnalysisRF.R
+++ R/Global/DataAnalysisRF.R
@@ -2,9 +2,9 @@
 ## Data were obtained from EPA and contractors from PCB Superfund
 ## sites in USA. Using log10 of the sum of PCB.
 ## Random forest model
+## Optimization is not used here due to very long run times.
 
 # Install packages
-install.packages("randomForest")
 install.packages("tidyverse")
 install.packages("ggplot2")
 install.packages("robustbase")
@@ -20,6 +20,7 @@ install.packages("scales")
 install.packages("sf")
 install.packages("units")
 install.packages("sfheaders")
+install.packages("gbm3")
 
 # Load libraries
 {
@@ -36,7 +37,7 @@ install.packages("sfheaders")
   library(patchwork) # combine plots
   library(sf) # Create file to be used in Google Earth
   library(units)
-  library(randomForest)
+  library(gbm3) # Random Forest functions
 }
 
 # Read data ---------------------------------------------------------------
@@ -48,86 +49,113 @@ wdc <- read.csv("Data/WaterDataCongenerAroclor09072023.csv")
   # Change date format
   SampleDate <- as.Date(wdc$SampleDate, format = "%m/%d/%y")
   # Calculate sampling time
-  time.day <- data.frame(as.Date(SampleDate) - min(as.Date(SampleDate)))
-  # Create individual code for each site sampled
-  site.numb <- wdc$SiteID %>% as.factor() %>% as.numeric
+  time.day <- as.numeric(as.Date(SampleDate),
+                                  min(as.Date(SampleDate), units = "days"))
   # Include season
   yq.s <- as.yearqtr(as.yearmon(wdc$SampleDate, "%m/%d/%Y") + 1/12)
   season.s <- factor(format(yq.s, "%q"), levels = 1:4,
                      labels = c("0", "S-1", "S-2", "S-3")) # winter, spring, summer, fall
   # Create data frame
-  tpcb <- cbind(factor(wdc$SiteID), SampleDate, wdc$tPCB,
-                data.frame(time.day), site.numb, season.s)
+  tpcb <- cbind(factor(wdc$SiteID), wdc$tPCB, data.frame(time.day), season.s)
   # Add column names
-  colnames(tpcb) <- c("SiteID", "date", "tPCB", "time", "site.code",
-                      "season")
+  colnames(tpcb) <- c("SiteID", "tPCB", "time", "season")
 }
 
-# Random Forest Model tPCB ------------------------------------------------
-# Train-Test Split
+# Random Forest Model tPCB ----------------------------------------------
+# Set seed for reproducibility
 set.seed(123)
+
+# Split data into training and test sets
 train_indices <- sample(1:nrow(tpcb), 0.8 * nrow(tpcb))
 train_data <- tpcb[train_indices, ]
 test_data <- tpcb[-train_indices, ]
 
-# Fit the Model (1)
-rf_model <- randomForest(log10(tPCB) ~ time + site.code + season,
-                           data = train_data)
+# Define hyperparameter grid
+param_grid <- expand.grid(
+  mtry = seq(1, ncol(train_data) - 1),  # Adjust mtry values based on your data
+  splitrule = c("gini", "extratrees"),
+  min.node.size = c(5, 10, 20)
+)
 
-# Make Predictions
-predictions <- predict(rf_model, newdata = test_data)
+# Prepare training control
+ctrl <- trainControl(method = "cv", number = 5, search = "grid")
 
-# Evaluate Model Performance
+# Perform grid search with cross-validation using ranger
+rf_model <- train(
+  log10(tPCB) ~ time + SiteID + season,
+  data = train_data,
+  method = "ranger",
+  importance = 'permutation',
+  tuneGrid = param_grid,
+  trControl = ctrl
+)
+
+# Get the best mtry
+best_mtry <- rf_model$bestTune$mtry
+
+final_rf_model <- ranger(
+  formula = log10(tPCB) ~ time + SiteID + season,
+  data = train_data,
+  num.trees = 5000, # need to manualy modify this parameter
+  mtry = best_mtry,
+  importance = 'permutation',
+  seed = 123
+)
+
+# Get predictions on the test data
+predictions <- predict(final_rf_model, data = test_data)$predictions
+
+# Evaluate model performance
 mse <- mean((predictions - log10(test_data$tPCB))^2)
 rmse <- sqrt(mse)
-r_squared <- 1 - (sum((log10(test_data$tPCB) - predictions)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
 
-# Estimate a factor of 2 between observations and predictions for the normal values
-# Create a data frame with observed and predicted values
-compare_df <- data.frame(observed = test_data$tPCB,
-                           predicted = 10^predictions)
+# Calculate R-squared
+ss_res <- sum((log10(test_data$tPCB) - predictions)^2)
+ss_tot <- sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2)
+r_squared <- 1 - (ss_res / ss_tot)
+
+# Print RMSE and R-squared
+print(paste("RMSE:", rmse))
+print(paste("R-squared:", r_squared))
 
 # Estimate a factor of 2 between observations and predictions
-compare_df$factor2 <- compare_df$observed/compare_df$predicted
+# Create a data frame with observed and predicted values
+comparison <- data.frame(observed = test_data$tPCB,
+                         predicted = 10^predictions)
+
+# Estimate a factor of 2 between observations and predictions
+comparison$factor2 <- comparison$observed/comparison$predicted
 
 # Calculate the percentage of observations within the factor of 2
-factor2_percentage <- nrow(compare_df[compare_df$factor2 > 0.5 & compare_df$factor2 < 2, ])/nrow(compare_df)*100
+factor2_percentage <- nrow(comparison[comparison$factor2 > 0.5 & comparison$factor2 < 2
+                                      , ])/nrow(comparison)*100
 
 # Create the data frame directly
-performance_df <- data.frame(Heading = c("RMSE", "R2", "Factor2"),
+performance_RF <- data.frame(Heading = c("RMSE", "R2", "Factor2"),
                              Value = c(rmse, r_squared,
                                        factor2_percentage))
-
-# Remove unnecessary columns
-performance_df <- performance_df[, !(names(performance_df) %in% c("V1", "V2", "V3"))]
-
 # Print the modified data frame
-print(performance_df)
+print(performance_RF)
 
 # Export results
-write.csv(performance_df,
-          file = "Output/Data/Global/csv/RFtPCB.csv")
+write.csv(performance_RF,
+          file = "Output/Data/Global/csv/RFtPCB.csv",
+          row.names = FALSE)
 
-# Feature Importance
-importance <- importance(rf_model)
-# Plot features
-barplot(importance[, 1], names.arg = rownames(importance),
-        main = "Feature Importance", las = 2, cex.names = 0.7)
-
-# Create a data frame for plotting
-plot_data.1 <- data.frame(
+# Create a data frame for plotting Observations vs Predictions
+plot_data <- data.frame(
   Location = rep("USA", nrow(test_data)),
-  Actual = log10(test_data$tPCB),
+  Observed = log10(test_data$tPCB),
   Predicted = predictions
 )
 
 # Export results
-write.csv(plot_data.1,
+write.csv(plot_data,
           file = "Output/Data/Global/csv/RFObsPredtPCB.csv",
           row.names = FALSE)
 
 # Create the scatter plot
-plotRF <- ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
+plotRF <- ggplot(plot_data, aes(x = 10^(Observed), y = 10^(Predicted))) +
   geom_point(shape = 21, size = 3, fill = "white") +
   scale_y_log10(limits = c(0.1, 10^8),
                 breaks = trans_breaks("log10", function(x) 10^x),
@@ -138,7 +166,8 @@ plotRF <- ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
   xlab(expression(bold("Observed concentration " *Sigma*"PCB (pg/L)"))) +
   ylab(expression(bold("Predicted concentration " *Sigma*"PCB (pg/L)"))) +
   geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
-  geom_abline(intercept = log10(2), slope = 1, col = "blue", linewidth = 0.7) + # 1:2 line (factor of 2)
+  geom_abline(intercept = log10(2), slope = 1, col = "blue",
+              linewidth = 0.7) + # 1:2 line (factor of 2)
   geom_abline(intercept = log10(0.5), slope = 1, col = "blue",
               linewidth = 0.7) + # 2:1 line (factor of 2)
   theme_bw() +
@@ -156,33 +185,30 @@ ggsave("Output/Plots/Global/RFtPCB.png",
 {
   # Only consider congener data
   wdc.pcb <- subset(wdc, AroclorCongener == "Congener")
-  # Create individual code for each site sampled
-  site.numb <- wdc.pcb$SiteID %>% as.factor() %>% as.numeric
   # Change date format
   SampleDate <- as.Date(wdc.pcb$SampleDate, format = "%m/%d/%y")
   # Calculate sampling time
-  time.day <- data.frame(as.Date(SampleDate) - min(as.Date(SampleDate)))
-  # Change name time.day to time
-  colnames(time.day) <- "time"
+  time.day <- as.numeric(difftime(as.Date(SampleDate),
+                                  min(as.Date(SampleDate)), units = "days"))
   # Include season
   yq.s <- as.yearqtr(as.yearmon(wdc.pcb$SampleDate, "%m/%d/%Y") + 1/12)
   season.s <- factor(format(yq.s, "%q"), levels = 1:4,
                      labels = c("0", "S-1", "S-2", "S-3")) # winter, spring, summer, fall
   # Remove metadata
-  wdc.pcb <- subset(wdc.pcb, select = -c(SampleID:AroclorCongener))
+  wdc.pcb.1 <- subset(wdc.pcb, select = -c(SampleID:AroclorCongener))
   # Remove Aroclor and tPCB data
-  wdc.pcb <- subset(wdc.pcb, select = -c(A1016:tPCB))
+  wdc.pcb.1 <- subset(wdc.pcb.1, select = -c(A1016:tPCB))
   # Log10 individual PCBs 
-  wdc.pcb <- log10(wdc.pcb)
+  wdc.pcb.1 <- log10(wdc.pcb.1)
   # Replace -inf to NA
-  wdc.pcb <- do.call(data.frame,
-                     lapply(wdc.pcb,
+  wdc.pcb.1 <- do.call(data.frame,
+                     lapply(wdc.pcb.1,
                             function(x) replace(x, is.infinite(x), NA)))
   # Remove individual PCB that have 30% or less NA values
-  wdc.pcb.1 <- wdc.pcb[,
-                       -which(colSums(is.na(wdc.pcb))/nrow(wdc.pcb) > 0.7)]
+  wdc.pcb.1 <- wdc.pcb.1[,
+                       -which(colSums(is.na(wdc.pcb.1))/nrow(wdc.pcb.1) > 0.7)]
   # Add date and time to wdc.pcb.1
-  wdc.pcb.1 <- cbind(wdc.pcb.1, data.frame(time.day), site.numb,
+  wdc.pcb.1 <- cbind(wdc.pcb.1, as.factor(wdc.pcb$SiteID), data.frame(time.day),
                      season.s)
 }
 
@@ -207,10 +233,10 @@ rf_results <- data.frame(
 all_results <- data.frame()
 
 # Iterate over each numeric column
-for (i in seq_along(pcb_numeric_columns)) {
+for (i in seq_along(rf_results$Congener)) {
   # Combine numeric and character data
-  combined_data <- cbind(wdc.pcb.1[, pcb_numeric_columns[i],
-                                   drop = FALSE], wdc.pcb.1[, char_columns])
+  combined_data <- cbind(wdc.pcb.1[, pcb_numeric_columns[i], drop = FALSE],
+                         wdc.pcb.1[, char_columns])
   
   # Exclude rows with missing values
   combined_data <- na.omit(combined_data)
@@ -222,28 +248,36 @@ for (i in seq_along(pcb_numeric_columns)) {
   train_data <- combined_data[train_indices, ]
   test_data <- combined_data[-train_indices, ]
   
-  # Modeling code using randomForest
-  fit <- randomForest(train_data[, 1] ~ ., data = train_data)
+  # Modeling code using ranger
+  rf_model <- ranger(
+    y = train_data[, 1],  # Response variable (first column)
+    x = train_data[, -1], # Predictor variables (all columns except the first)
+    num.trees = 5000,    # Adjust num.trees as needed
+    importance = 'permutation',
+    seed = 123
+  )
   
-  # Example: Make predictions on the test set
-  predictions <- predict(fit, newdata = test_data)
+  # Get predictions on the test set
+  predictions <- predict(rf_model, data = test_data)$predictions
   
-  # Calculate mean squared error (mse) for illustration
+  # Calculate mean squared error (mse)
   mse <- mean((predictions - test_data[, 1])^2)
   
   # Calculate R-squared
-  R_squared <- 1 - (sum((test_data[, 1] - predictions)^2) / sum((test_data[, 1] - mean(test_data[, 1]))^2))
+  ss_res <- sum((test_data[, 1] - predictions)^2)
+  ss_tot <- sum((test_data[, 1] - mean(test_data[, 1]))^2)
+  r_squared <- 1 - (ss_res / ss_tot)
   
-  # Calculate factor2_percentage within the loop with normal scale
+  # Calculate factor2_percentage within the loop
   compare_df <- data.frame(
-    observed = 10^(test_data[, 1]),
-    predicted = 10^(predictions)
+    observed = test_data[, 1],
+    predicted = predictions
   )
   compare_df$factor2 <- compare_df$observed / compare_df$predicted
   factor2_percentage <- sum(compare_df$factor2 > 0.5 & compare_df$factor2 < 2) / nrow(compare_df) * 100
   
   # Store the results in the matrix
-  rf_results[i, 2:4] <- c(mse, R_squared, factor2_percentage)
+  rf_results[i, 2:4] <- c(sqrt(mse), r_squared, factor2_percentage)
   
   # Create a data frame for each column's results
   col_results <- data.frame(
@@ -251,7 +285,7 @@ for (i in seq_along(pcb_numeric_columns)) {
     Congener = rep(pcb_numeric_columns[i], length(test_data[, 1])),
     Actual = test_data[, 1],
     Predicted = predictions,
-    R_squared = R_squared  # Add R_squared
+    R_squared = r_squared  # Add R_squared column
   )
   
   # Bind the data frame to the overall results
@@ -291,24 +325,11 @@ plotRFPCBi <- ggplot(all_results, aes(x = 10^(Actual), y = 10^(Predicted))) +
   xlab(expression(bold("Observed concentration PCBi (pg/L)"))) +
   ylab(expression(bold("Predicted concentration PCBi (pg/L)"))) +
   geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
-  geom_abline(intercept = log10(2), slope = 1, col = "blue", linewidth = 0.7) + # 1:2 line (factor of 2)
-  geom_abline(intercept = log10(0.5), slope = 1, col = "blue", linewidth = 0.7) + # 2:1 line (factor of 2)
+  geom_abline(intercept = 0.30103, slope = 1, col = "blue", linewidth = 0.7) + # 1:2 line (factor of 2)
+  geom_abline(intercept = -0.30103, slope = 1, col = "blue", linewidth = 0.7) + # 2:1 line (factor of 2)
   theme_bw() +
   theme(aspect.ratio = 15/15) +
   annotation_logticks(sides = "bl")
-
-# To check values inside the 2:1 and 2:1 lines
-# Build the plot
-plot_data <- ggplot_build(plotRFPCBi)$data[[1]]
-# Count points inside the 2 lines
-points_inside_lines <- sum(plot_data$y >= plot_data$x - log10(2) & plot_data$y <= plot_data$x + log10(2))
-# Print the count
-print(points_inside_lines)
-
-# Count points outside the 2 lines
-points_outside_lines <- sum(plot_data$y < plot_data$x - log10(2) | plot_data$y > plot_data$x + log10(2))
-# Print the count
-print(points_outside_lines)
 
 # Print the plot
 print(plotRFPCBi)
