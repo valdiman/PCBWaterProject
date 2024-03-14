1324c460491599ccc4b613139b4d3ccd64249adc
martie <andres-martinez@uiowa.edu>
2024-03-14 14:05:47 -0500

    new version.

--- R/Sites/Analysis/RichardsonRF.R
+++ R/Sites/Analysis/RichardsonRF.R
@@ -4,7 +4,6 @@
 ## Random forest model
 
 # Install packages
-install.packages("randomForest")
 install.packages("tidyverse")
 install.packages("ggplot2")
 install.packages("robustbase")
@@ -20,6 +19,8 @@ install.packages("scales")
 install.packages("sf")
 install.packages("units")
 install.packages("sfheaders")
+install.packages('ranger')
+install.packages('caret')
 
 # Load libraries
 {
@@ -36,7 +37,8 @@ install.packages("sfheaders")
   library(patchwork) # combine plots
   library(sf) # Create file to be used in Google Earth
   library(units)
-  library(randomForest)
+  library(ranger) # Random Forest functions
+  library(caret) # For cross-validation
 }
 
 # Read data ---------------------------------------------------------------
@@ -82,89 +84,117 @@ rhr <- wdc[str_detect(wdc$LocationName, 'Richardson Hill Road Landfill'),]
   # Change date format
   rhr$SampleDate <- as.Date(rhr$SampleDate, format = "%m/%d/%y")
   # Calculate sampling time
-  time.day <- data.frame(as.Date(rhr$SampleDate) - min(as.Date(rhr$SampleDate)))
-  # Create individual code for each site sampled
-  site.numb <- rhr$SiteID %>% as.factor() %>% as.numeric
+  time.day <- as.numeric(difftime(as.Date(rhr$SampleDate),
+                                  min(as.Date(rhr$SampleDate)), units = "days"))
   # Include season
   yq.s <- as.yearqtr(as.yearmon(rhr$SampleDate, "%m/%d/%Y") + 1/12)
   season.s <- factor(format(yq.s, "%q"), levels = 1:4,
                      labels = c("0", "S-1", "S-2", "S-3")) # winter, spring, summer, fall
   # Create data frame
-  rhr.tpcb <- cbind(factor(rhr$SiteID), rhr$SampleDate, as.matrix(rhr$tPCB),
-                    data.frame(time.day), site.numb, season.s,
-                    rhr$DistanceToSouthernLocation)
+  rhr.tpcb <- cbind(factor(rhr$SiteID), as.matrix(rhr$tPCB), data.frame(time.day),
+                    season.s, rhr$DistanceToSouthernLocation)
   # Add column names
-  colnames(rhr.tpcb) <- c("SiteID", "date", "tPCB", "time", "site.code",
-                          "season", "DistanceToSouthernLocation")
+  colnames(rhr.tpcb) <- c("SiteID", "tPCB", "time", "season",
+                          "DistanceToSouthernLocation")
 }
 
-# Random Forest Model -----------------------------------------------------
-# Train-Test Split
+# Random Forest Model tPCB ------------------------------------------------
+# Set seed for reproducibility
 set.seed(123)
+
+# Train-Test Split
 train_indices <- sample(1:nrow(rhr.tpcb), 0.8 * nrow(rhr.tpcb))
 train_data <- rhr.tpcb[train_indices, ]
 test_data <- rhr.tpcb[-train_indices, ]
 
-# Fit the Model (1)
-rf_model.1 <- randomForest(log10(tPCB) ~ time + site.code + season +
-                             DistanceToSouthernLocation, data = train_data)
+# Define hyperparameter grid
+param_grid <- expand.grid(
+  mtry = seq(1, ncol(train_data) - 1),  # Adjust mtry values based on your data
+  splitrule = c("gini", "extratrees"),
+  min.node.size = c(2, 3, 5)
+)
 
-# Make Predictions
-predictions.1 <- predict(rf_model.1, newdata = test_data)
+# Prepare training control
+ctrl <- trainControl(method = "cv", number = 5, search = "grid")
 
-# Evaluate Model Performance
-mse.1 <- mean((predictions.1 - log10(test_data$tPCB))^2)
-rmse.1 <- sqrt(mse.1)
-r_squared.1 <- 1 - (sum((log10(test_data$tPCB) - predictions.1)^2)/sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2))
+# Perform grid search with cross-validation using ranger
+rf_model <- train(
+  log10(tPCB) ~ time + SiteID + season + DistanceToSouthernLocation,
+  data = train_data,
+  method = "ranger",
+  importance = 'permutation',
+  tuneGrid = param_grid,
+  trControl = ctrl
+)
+
+# Get the best mtry
+best_mtry <- rf_model$bestTune$mtry
+
+final_rf_model <- ranger(
+  formula = log10(tPCB) ~ time + SiteID + season + DistanceToSouthernLocation,
+  data = train_data,
+  num.trees = 100, # need to manually modify this parameter
+  mtry = best_mtry,
+  importance = 'permutation',
+  seed = 123
+)
+
+# Get predictions on the test data
+predictions <- predict(final_rf_model, data = test_data)$predictions
+
+# Evaluate model performance
+mse <- mean((predictions - log10(test_data$tPCB))^2)
+rmse <- sqrt(mse)
+
+# Calculate R-squared
+ss_res <- sum((log10(test_data$tPCB) - predictions)^2)
+ss_tot <- sum((log10(test_data$tPCB) - mean(log10(test_data$tPCB)))^2)
+r_squared <- 1 - (ss_res / ss_tot)
+
+# Print RMSE and R-squared
+print(paste("RMSE:", rmse))
+print(paste("R-squared:", r_squared))
 
 # Estimate a factor of 2 between observations and predictions
 # Create a data frame with observed and predicted values
-compare_df.1 <- data.frame(observed = test_data$tPCB,
-                           predicted = 10^predictions.1)
+comparison <- data.frame(observed = test_data$tPCB,
+                         predicted = 10^predictions)
 
 # Estimate a factor of 2 between observations and predictions
-compare_df.1$factor2 <- compare_df.1$observed/compare_df.1$predicted
+comparison$factor2 <- comparison$observed/comparison$predicted
 
 # Calculate the percentage of observations within the factor of 2
-factor2_percentage.1 <- nrow(compare_df.1[compare_df.1$factor2 > 0.5 & compare_df.1$factor2 < 2, ])/nrow(compare_df.1)*100
+factor2_percentage <- nrow(comparison[comparison$factor2 > 0.5 & comparison$factor2 < 2
+                                      , ])/nrow(comparison)*100
+
+# Print Factor2
+print(paste("Factor2:", factor2_percentage))
 
 # Create the data frame directly
-performance_df <- data.frame(Heading = c("RMSE", "R2", "Factor2"),
-                             Value = c(rmse.1, r_squared.1,
-                                       factor2_percentage.1))
-
-# Remove unnecessary columns
-performance_df <- performance_df[, !(names(performance_df) %in% c("V1", "V2", "V3"))]
-
-# Print the modified data frame
-print(performance_df)
+performance_RF <- data.frame(Heading = c("RMSE", "R2", "Factor2"),
+                             Value = c(rmse, r_squared,
+                                       factor2_percentage))
 
 # Export results
-write.csv(performance_df,
-          file = "Output/Data/Sites/csv/Richardson/RichardsonRFPerformancetPCB.csv",
+write.csv(performance_RF,
+          file = "Output/Data/Sites/csv/Richardson/RichardsonRFtPCB.csv",
           row.names = FALSE)
 
-# Feature Importance
-importance.1 <- importance(rf_model.1)
-# Plot features
-barplot(importance.1[, 1], names.arg = rownames(importance.1),
-        main = "Feature Importance", las = 2, cex.names = 0.7)
-
 # Create a data frame for plotting
-plot_data.1 <- data.frame(
+plot_data <- data.frame(
   Location = rep("Richardson Hill Road Landfill", nrow(test_data)),
   Actual = log10(test_data$tPCB),
-  Predicted = predictions.1
+  Predicted = predictions
 )
 
 # Export results
-write.csv(plot_data.1,
+write.csv(plot_data,
           file = "Output/Data/Sites/csv/Richardson/RichardsonRFObsPredtPCB.csv",
           row.names = FALSE)
 
 # Create the scatter plot
-plotRF <- ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
-  geom_point(shape = 21, size = 3, fill = "white") +
+plotRF <- ggplot(plot_data, aes(x = 10^(Actual), y = 10^(Predicted))) +
+  geom_point(shape = 21, size = 1, fill = "white") +
   scale_y_log10(limits = c(100, 10^7),
                 breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x))) +
@@ -174,9 +204,9 @@ plotRF <- ggplot(plot_data.1, aes(x = 10^(Actual), y = 10^(Predicted))) +
   xlab(expression(bold("Observed concentration " *Sigma*"PCB (pg/L)"))) +
   ylab(expression(bold("Predicted lme concentration " *Sigma*"PCB (pg/L)"))) +
   geom_abline(intercept = 0, slope = 1, col = "black", linewidth = 0.7) +
-  geom_abline(intercept = 0.30103, slope = 1, col = "blue",
+  geom_abline(intercept = log10(2), slope = 1, col = "blue",
               linewidth = 0.7) + # 1:2 line (factor of 2)
-  geom_abline(intercept = -0.30103, slope = 1, col = "blue",
+  geom_abline(intercept = log10(0.5), slope = 1, col = "blue",
               linewidth = 0.7) + # 2:1 line (factor of 2)
   theme_bw() +
   theme(aspect.ratio = 15/15) +
@@ -188,3 +218,5 @@ print(plotRF)
 # Save plot in folder
 ggsave("Output/Plots/Sites/ObsPred/Richardson/RichardsonRFtPCB.png",
        plot = plotRF, width = 6, height = 5, dpi = 500)
+
+# It doesn't work. R2 < 0.0
